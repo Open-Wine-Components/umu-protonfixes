c2531db3c65e80091bcbe7947edba523b9fa30eb
GloriousEggroll <gloriouseggroll@gmail.com>
2019-11-18 01:27:40 -0700

    add mvc ultimate

---
+++ gamefixes/357190.py
@@ -0,0 +1,12 @@
+""" Ultimate Marvel vs. Capcom 3
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Requires media foundation dlls and mem_alloc mod
+    """
+
+    util.protontricks('mf_install')
+
