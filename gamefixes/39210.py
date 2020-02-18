99f4239c2517fdbd4b43f7640063b9012af6799a
GloriousEggroll <gloriouseggroll@gmail.com>
2020-02-17 22:14:42 -0700

    workaround for ffxiv launcher

--- gamefixes/39210.py
+++ gamefixes/39210.py
@@ -1,4 +1,4 @@
-""" Game fix for FFXIV
+""" Game fix for FFXIV Trial
 """
 #pylint: disable=C0103
 
@@ -8,6 +8,8 @@ import os
 def main():
     """ for FFXIV skip intro cutscene to allow game to work.
     """
+    # Fixes the startup process.
+    util.replace_command('ffxivboot.exe', 'ffxivboot64.exe')
 
     # disable new character intro cutscene to prevent black screen loop
     configpath = os.path.join(util.protonprefix(), 'drive_c/users/steamuser/My Documents/My Games/FINAL FANTASY XIV - A Realm Reborn')
@@ -18,5 +20,11 @@ def main():
         f = open(configgame,"w+")
         f.write("<FINAL FANTASY XIV Config File>\n\n<Cutscene Settings>\nCutsceneMovieOpening 1")
         f.close
-
-
+    configpath = os.path.join(util.protonprefix(), 'drive_c/users/steamuser/My Documents/My Games/FINAL FANTASY XIV - A Realm Reborn')
+    if not os.path.exists(configpath):
+        os.makedirs(configpath)
+    configgame = os.path.join(configpath, 'FFXIV_BOOT.cfg')
+    if not os.path.isfile(configgame):
+        f = open(configgame,"w+")
+        f.write("<FINAL FANTASY XIV Boot Config File>\n\n<Version>\nBrowser 1")
+        f.close
