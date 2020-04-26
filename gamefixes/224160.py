e9c17c61e3676ea970a065f54df0ae004e60692a
GloriousEggroll <gloriouseggroll@gmail.com>
2020-04-26 16:52:40 -0600

    homeworld remastered needs dotnet40

---
+++ gamefixes/224160.py
@@ -0,0 +1,12 @@
+""" Game fix Homeworld Remastered Collection
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Needs dotnet40 """
+
+    util.protontricks('dotnet40')
+
+#TODO Controllers fixes
