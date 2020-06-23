d1fa190c960b681fe2c6bf1bbbf470d3d215069c
GloriousEggroll <gloriouseggroll@gmail.com>
2020-06-22 20:57:43 -0600

    protonfixes: add launcher fix for mount and blade bannerlord

--- gamefixes/261550.py
+++ gamefixes/261550.py
@@ -2,6 +2,8 @@
 """
 #pylint: disable=C0103
 
+import os
+import subprocess
 from protonfixes import util
 
 def main():
@@ -9,7 +11,13 @@ def main():
     """
 
     # Fixes the startup process.
-    util.replace_command('TaleWorlds.MountAndBlade.Launcher.exe', 'Bannerlord.exe')
     util.set_environment('WINEDLLOVERRIDES','dxgi=n')
     util.set_environment('DXVK_ASYNC','1')
 
+    installpath = os.getcwd()
+
+    if not os.path.isfile(os.path.join(os.path.abspath(installpath), 'bin', 'Win64_Shipping_Client', 'ManagedStarter.exe')):
+        subprocess.call(['ln', '-s', 'Bannerlord.exe', 'ManagedStarter.exe'])
+
+    if not os.path.isfile(os.path.join(os.path.abspath(installpath),  'bin', 'Win64_Shipping_Client', 'ManagedStarter_BE.exe')):
+        subprocess.call(['ln', '-s', 'Bannerlord_BE.exe', 'ManagedStarter_BE.exe'])
