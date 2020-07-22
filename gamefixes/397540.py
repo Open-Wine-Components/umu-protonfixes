ab61f7424d0f91498c396f4c01dc6a312af38829
Thomas Crider <gloriouseggroll@gmail.com>
2020-07-21 19:17:06 -0600

    add -NoLauncher -notexturestreaming to bl3 launch options

---
+++ gamefixes/397540.py
@@ -0,0 +1,12 @@
+""" Game fix for Path of Exile
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Enable preload options
+    """
+    # Enable preload options
+    util.append_argument('-NoLauncher -notexturestreaming')
+
