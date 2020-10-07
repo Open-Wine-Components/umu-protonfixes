c10b7c4bd037e0e9d2fee2ce573fcf69f73c9074
GloriousEggroll <gloriouseggroll@gmail.com>
2020-10-07 00:38:54 -0600

    add Baldur's Gate 3 protonfix

---
+++ gamefixes/1086940.py
@@ -0,0 +1,13 @@
+""" Game fix for Baldur's Gate 3
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Run script extender if it exists.
+    """
+    # Fixes the startup process.
+    util.protontricks('vcrun2019_ge')
+    util.protontricks('d3dcompiler_47')
+    util.replace_command('LariLauncher.exe', '../bin/bg3.exe')
