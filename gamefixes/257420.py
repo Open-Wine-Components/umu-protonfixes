e70798035f45ad4f42a22efee3c4be983be9a4a3
GloriousEggroll <gloriouseggroll@gmail.com>
2020-10-11 05:36:28 -0600

    SS4: give users choice between dx11 or vulkan

--- gamefixes/257420.py
+++ gamefixes/257420.py
@@ -5,16 +5,18 @@
 import os
 import subprocess
 from protonfixes import util
+from protonfixes import splash
 
 def main():
-    """ This launches the game with the DX11 renderer by default.
+    """ Graphics API workaround
     """
     util.protontricks('d3dcompiler_47')
     if not os.path.isfile('UserCfg.lua.bak'):
         subprocess.call(['cp', 'UserCfg.lua', 'UserCfg.lua.bak'])
         f = open('UserCfg.lua',"a+")
-        f.write("gfx_strAPI = \"Direct3D11\";\nsfx_strAPI = \"OpenAL\";\nren_bDepthPrepass = 0;")
+        f.write("gfx_strAPI = \"Vulkan\";\nsfx_strAPI = \"OpenAL\";\nren_bDepthPrepass = 0;")
         f.close
+
     if not os.path.isfile('Content/SeriousSam4/Config/Content/SeriousSam4/Config/CheckDriver.lua.bak'):
         subprocess.call(['cp', 'Content/SeriousSam4/Config/CheckDriver.lua', 'Content/SeriousSam4/Config/CheckDriver.lua.bak'])
         f = open('Content/SeriousSam4/Config/CheckDriver.lua',"rt")
@@ -24,3 +26,49 @@ def main():
         f = open('Content/SeriousSam4/Config/CheckDriver.lua',"wt")
         f.write(data)
         f.close()
+
+    zenity_bin = splash.sys_zenity_path()
+    if not zenity_bin:
+        return
+    zenity_cmd = ' '.join([zenity_bin, '--question','--text', '"Would you like to run the game with Vulkan? (No = DX11)"', '--no-wrap'])
+    zenity = subprocess.Popen(zenity_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    zenity.communicate()
+    if zenity.returncode:
+        util.set_environment('WINEDLLOVERRIDES','dxgi=n')
+        f = open("UserCfg.lua", "r")
+        for line in f:
+            if "Vulkan" in line:
+                f = open('UserCfg.lua',"rt")
+                data = f.read()
+                data = data.replace('gfx_strAPI = "Vulkan";', 'gfx_strAPI = "Direct3D11";')
+                f.close()
+                f = open('UserCfg.lua',"wt")
+                f.write(data)
+                f.close()
+            if "Direct3D12" in line:
+                f = open('UserCfg.lua',"rt")
+                data = f.read()
+                data = data.replace('gfx_strAPI = "Direct3D12";', 'gfx_strAPI = "Direct3D11";')
+                f.close()
+                f = open('UserCfg.lua',"wt")
+                f.write(data)
+                f.close()
+    else:
+        f = open("UserCfg.lua", "r")
+        for line in f:
+            if "Direct3D11" in line:
+                f = open('UserCfg.lua',"rt")
+                data = f.read()
+                data = data.replace('gfx_strAPI = "Direct3D11";', 'gfx_strAPI = "Vulkan";')
+                f.close()
+                f = open('UserCfg.lua',"wt")
+                f.write(data)
+                f.close()
+            if "Direct3D12" in line:
+                f = open('UserCfg.lua',"rt")
+                data = f.read()
+                data = data.replace('gfx_strAPI = "Direct3D12";', 'gfx_strAPI = "Vulkan";')
+                f.close()
+                f = open('UserCfg.lua',"wt")
+                f.write(data)
+                f.close()
