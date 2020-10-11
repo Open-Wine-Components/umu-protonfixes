35ac193cafa6947009d35593d28a5ec2f6fe0655
GloriousEggroll <gloriouseggroll@gmail.com>
2020-10-11 05:36:48 -0600

    Baldur's Gate 3: give users a choice between dx11 or vulkan

--- gamefixes/1086940.py
+++ gamefixes/1086940.py
@@ -3,11 +3,23 @@
 #pylint: disable=C0103
 
 from protonfixes import util
+from protonfixes import splash
+import subprocess
+
 
 def main():
-    """ Run script extender if it exists.
+    """ Launcher workaround
     """
     # Fixes the startup process.
     util.protontricks('vcrun2019_ge')
     util.protontricks('d3dcompiler_47')
     util.replace_command('LariLauncher.exe', '../bin/bg3.exe')
+    zenity_bin = splash.sys_zenity_path()
+    if not zenity_bin:
+        return
+    zenity_cmd = ' '.join([zenity_bin, '--question','--text', '"Would you like to run the game with Vulkan? (No = DX11)"', '--no-wrap'])
+    zenity = subprocess.Popen(zenity_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    zenity.communicate()
+    if zenity.returncode:
+        util.replace_command('../bin/bg3.exe', '../bin/bg3_dx11.exe')
+        util.set_environment('WINEDLLOVERRIDES','dxgi=n')
