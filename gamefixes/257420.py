ff9f06ef91f0cbeda44064747a371974dd954e51
Thomas Crider <gloriouseggroll@gmail.com>
2020-11-17 02:30:41 -0700

    simplify ss4 protonfixes now that flicker is fixed

--- gamefixes/257420.py
+++ gamefixes/257420.py
@@ -9,20 +9,8 @@ from protonfixes import util
 def main():
     """ Graphics API workaround
     """
-    util.protontricks('d3dcompiler_47')
     if not os.path.isfile('UserCfg.lua.bak'):
         subprocess.call(['cp', 'UserCfg.lua', 'UserCfg.lua.bak'])
         f = open('UserCfg.lua',"a+")
-        f.write("gfx_strAPI = \"Vulkan\";\nsfx_strAPI = \"OpenAL\";\nren_bDepthPrepass = 0;")
+        f.write("gfx_strAPI = \"Vulkan\";\nsfx_strAPI = \"OpenAL\";")
         f.close
-
-    if not os.path.isfile('Content/SeriousSam4/Config/Content/SeriousSam4/Config/CheckDriver.lua.bak'):
-        subprocess.call(['cp', 'Content/SeriousSam4/Config/CheckDriver.lua', 'Content/SeriousSam4/Config/CheckDriver.lua.bak'])
-        f = open('Content/SeriousSam4/Config/CheckDriver.lua',"rt")
-        data = f.read()
-        data = data.replace('gfx_iReqDriverVersion = 1100;', 'gfx_iReqDriverVersion = 1000;')
-        f.close()
-        f = open('Content/SeriousSam4/Config/CheckDriver.lua',"wt")
-        f.write(data)
-        f.close()
-
