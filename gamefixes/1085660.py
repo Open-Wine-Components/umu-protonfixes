31813c0ba6911ae9a963381547ac6f4edc285dc9
Thomas Crider <gloriouseggroll@gmail.com>
2021-01-30 17:05:14 -0700

    add vcrun2019 override for destiny 2 (game still does not work yet)

--- gamefixes/1085660.py
+++ gamefixes/1085660.py
@@ -1,12 +1,10 @@
-""" Game fix for Destiny 2
+""" Game fix Destiny 2
 """
 #pylint: disable=C0103
 
 from protonfixes import util
 
 def main():
-    """ installs vcrun2019
-    """
-
+    # Requires vcrun2019 to launch
     util.protontricks('vcrun2019_ge')
-    util.set_environment('DXVK_LOG_PATH','none')
+    util.set_environment('WINEDLLOVERRIDES','dxgi=n')
