ac9f0346c26c51d8108ccf7d4fe53161da5077b9
GloriousEggroll <gloriouseggroll@gmail.com>
2021-05-09 21:24:12 -0600

    installing quartz last seems to fix p4g from not being able to relaunch

--- gamefixes/1113000.py
+++ gamefixes/1113000.py
@@ -11,9 +11,9 @@ def main():
     # Fix pre-rendered cutscene playback
     util.protontricks('xactengine3_7_ge')
     util.protontricks('wmp9_ge')
-    util.protontricks('quartz')
     util.protontricks('d3dx11_43')
     util.protontricks('d3dcompiler_43')
     util.protontricks('klite')
+    util.protontricks('quartz')
     util.set_environment('WINEDLLOVERRIDES','mfplay=n')
 
