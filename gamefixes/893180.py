01b36e73a08159bf30a0b41775bb6a6b9e0bdba1
GloriousEggroll <gloriouseggroll@gmail.com>
2021-05-16 21:04:01 -0600

    catherine classic needs d3dcompiler fixes in order to open

---
+++ gamefixes/893180.py
@@ -0,0 +1,11 @@
+""" Game fix Catherine Classic
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """
+    """
+    util.protontricks('d3dcompiler_43')
+    util.protontricks('d3dcompiler_47')
