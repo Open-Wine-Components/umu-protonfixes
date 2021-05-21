726ecabab66b2c13159f590d70777a51dcdb9c74
GloriousEggroll <gloriouseggroll@gmail.com>
2021-05-21 16:37:13 -0600

    avengers works now, no need to disable dx12

--- gamefixes/997070.py
+++ gamefixes/997070.py
@@ -8,9 +8,3 @@ def main():
     # Requires vcrun2019 to launch
     util.protontricks('vcrun2019_ge')
     util.protontricks('d3dcompiler_47')
-    util.use_seccomp()
-    util.set_environment('PROTON_NO_D3D12','1')
-    util.set_environment('WINEDLLOVERRIDES','dxgi=n')
-
-
- 
