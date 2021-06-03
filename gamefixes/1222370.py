dd654912ae4b9d34788d9712dfa06a185e6a69fa
Thomas Crider <gloriouseggroll@gmail.com>
2021-06-03 00:44:18 -0600

    add UE4 preinstall skip fix for Necromunda

---
+++ gamefixes/1222370.py
@@ -0,0 +1,11 @@
+""" Necromunda: Hired Gun
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ 
+    """
+
+    util.replace_command('Necromunda.exe', 'Necromunda/Binaries/Win64/Necromunda-Win64-Shipping.exe')
