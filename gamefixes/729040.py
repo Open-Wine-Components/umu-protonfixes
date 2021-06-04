72439d3659b01a34cdca53d822a66554a0019d76
Bringas4You <49424816+iWeaker4you@users.noreply.github.com>
2021-06-04 12:25:07 -0700

    Skip Borderlands GOTY launcher

---
+++ gamefixes/729040.py
@@ -0,0 +1,11 @@
+""" Borderlands GOTY 
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ 
+    """
+
+    util.replace_command('Launcher.exe', 'BorderlandsGOTY.exe')
