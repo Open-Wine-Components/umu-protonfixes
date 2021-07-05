88328a46585153995a9d808d6edf160c38a69dbe
Thomas Crider <gloriouseggroll@gmail.com>
2021-07-05 16:27:04 -0600

    add fixes for star wars galactic battlegrounds saga

---
+++ gamefixes/356500.py
@@ -0,0 +1,10 @@
+""" Game fix for STAR WARS Galactic Battlegrounds Saga
+"""
+
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    util.protontricks('icodecs')
+    util.protontricks('directplay')
