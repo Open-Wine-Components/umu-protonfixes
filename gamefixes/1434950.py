c1f8e77586b633c343724d3a99eaac4bd08747a2
Thomas Crider <gloriouseggroll@gmail.com>
2021-07-29 22:59:45 -0600

    add HighFleet d3d requirements

---
+++ gamefixes/1434950.py
@@ -0,0 +1,12 @@
+""" Game fix HighFleet
+"""
+#pylint: disable=C0103
+#
+from protonfixes import util
+
+def main():
+    """ installs d3dcompiler, d3dx11_43
+    """
+
+    util.protontricks('d3dx11_43')
+    util.protontricks('d3dcompiler_47')
