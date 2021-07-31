1788cd8eb0f4338be81a57c9ef536be086682b29
Manuel Iglesias García <55031066+manueliglesiasgarcia@users.noreply.github.com>
2021-07-31 17:57:21 +0100

    Update 110800.py
    Remove invalid winetrick

--- gamefixes/110800.py
+++ gamefixes/110800.py
@@ -9,7 +9,6 @@ def main():
     """
 
     # https://github.com/ValveSoftware/Proton/issues/544#issuecomment-826150012
-    util.protontricks('110800')
     util.protontricks('d3dx9_43')
     util.protontricks('d3dcompiler_43')
     util.protontricks('d3dx11_43')
