7f2d926d372e4597b01ebc22bd285fea2001d842
Alejandro Bringas Martinez <49424816+iWeaker4you@users.noreply.github.com>
2021-08-26 01:27:13 +0000

    Create stuttering fix for FFXIII
    Final Fantasy XIII has a stuttering problem within the game, with DXVK_ASYNC it improves it a lot without having a visual problem, compared to FFXIII-2 that when enable DXVK_ASYNC it suffers a serious problem of "pop-in" or visual glitches

---
+++ gamefixes/292120.py
@@ -0,0 +1,13 @@
+
+""" Game fix for Final Fantasy XIII 
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Changes the proton argument from the launcher to the game
+    """
+
+    # Replace launcher with game exe in proton arguments
+    util.enable_dxvk_async()
