3dc18ed0ab672f28bc362a0b13a60441faf53987
Thomas Crider <gloriouseggroll@gmail.com>
2021-09-25 16:23:48 -0600

    update bejeweled 3 fix

--- gamefixes/78000.py
+++ gamefixes/78000.py
@@ -5,7 +5,6 @@
 from protonfixes import util
 
 def main():
-    """ disable dxvk
+    """ game needs d3dcompiler_43 for dx10 mode, but still does not work. disabling dx10 mode allows dx9 mode to work with dxvk
     """
-
-    util.disable_dxvk()
+    util.set_environment('PROTON_NO_D3D10', '1')
