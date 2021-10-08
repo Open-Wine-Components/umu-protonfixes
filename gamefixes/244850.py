53ed048be00f96db9a6c0ebf0b7eddd022949dec
Manuel Iglesias García <55031066+manueliglesiasgarcia@users.noreply.github.com>
2021-10-08 03:19:21 +0100

    Add parse XML and add gcServer  (244850) patch

--- gamefixes/244850.py
+++ gamefixes/244850.py
@@ -7,6 +7,14 @@ from protonfixes import util
 
 
 def main():
+
+    base_attibutte = "<runtime>"
+    game_opts = """
+  	<gcServer enabled = "true" />
+"""
+
+    util.set_xml_options(base_attibutte, game_opts, 'SpaceEngineers.exe.config','game')
+
     # This requires Proton 5.0 installed
     util.install_dotnet('dotnet48')
     util.append_argument('-skipintro')
