2f3b70522b88646fa30f215462fd0650a6df0399
Thomas Crider <gloriouseggroll@gmail.com>
2021-10-10 23:58:03 -0600

    add audio fix for apex legends (fixes game crash at opening)

---
+++ gamefixes/1172470.py
@@ -0,0 +1,12 @@
+""" Game fix for Apex Legends
+"""
+
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    util.protontricks('d3dcompiler_43')
+    util.protontricks('d3dx9_43')
+    util.protontricks('d3dx11_43')
+    util.protontricks('xact')
