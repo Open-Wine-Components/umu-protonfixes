bb41bb16565e7dbeea5f32619188b474c749ba69
PolisanTheEasyNick <polisanoleg@gmail.com>
2021-10-13 00:36:16 +0300

    Add support for Assasin's Creed: Odyssey

---
+++ gamefixes/812140.py
@@ -0,0 +1,19 @@
+""" Assassin's Creed: Odyssey
+"""
+import os
+from protonfixes import util
+
+def main():
+    """ disable Easy Anti-Cheat and online play, disable uplay overlay and change closebehavior
+    """
+
+    uplayconfigpath = os.path.join(util.protonprefix(), 'drive_c/users/steamuser/Local Settings/Application Data/Ubisoft Game Launcher')
+    if not os.path.exists(uplayconfigpath):
+        os.makedirs(uplayconfigpath)
+    uplayconfigfile = os.path.join(uplayconfigpath, 'settings.yml')
+    if not os.path.isfile(uplayconfigfile):
+        f = open(uplayconfigfile,"w+")
+        f.write("overlay:\n  enabled: false\n  fps_enabled: false\n  warning_enabled: false\nuser:\n  closebehavior: CloseBehavior_Close\n  landingpage: LandingPageLastPlayedGame\n")
+        f.close
+    # Replace launcher with game exe in proton arguments
+    util.append_argument('-eac_launcher -nosplash')
