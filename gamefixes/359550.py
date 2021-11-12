72c0c3754595a2d36bde4a07a1e63edded1bc411
Thomas Crider <gloriouseggroll@gmail.com>
2021-11-12 01:18:25 -0700

    update r6s gamefix

--- gamefixes/359550.py
+++ gamefixes/359550.py
@@ -5,8 +5,8 @@
 from protonfixes import util
 
 def main():
-    """ Rainbow Six Siege needs vk_x11_override_min_image_count=2 for AMD
+    """ Rainbow Six Siege needs vk_x11_override_min_image_count=2 for AMD, and overlay disabled for Vulkan
     """
 
-    # https://github.com/ValveSoftware/Proton/issues/200#issuecomment-415905979
+    util.disable_uplay_overlay()
     util.set_environment('vk_x11_override_min_image_count', '2')
