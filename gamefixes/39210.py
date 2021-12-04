03168692c29ad0e4cd796004adcc5d201dd6f416
Thomas Crider <gloriouseggroll@gmail.com>
2021-12-04 13:43:57 -0500

    FFXIV Play button and opening cutscene are now fixed, we also dont need hidewineexports, and launcher fixed moved to main proton script

--- gamefixes/39210.py
+++ gamefixes/39210.py
@@ -6,26 +6,8 @@ from protonfixes import util
 import os
 
 def main():
-    """ for FFXIV skip intro cutscene to allow game to work.
+    """ FFXIV add NOSTEAM option.
     """
-    util.protontricks('hidewineexports=enable')
-    # disable new character intro cutscene to prevent black screen loop
-    configpath = os.path.join(util.protonprefix(), 'drive_c/users/steamuser/Documents/My Games/FINAL FANTASY XIV - A Realm Reborn')
-    if not os.path.exists(configpath):
-        os.makedirs(configpath)
-    configgame = os.path.join(configpath, 'FFXIV.cfg')
-    if not os.path.isfile(configgame):
-        f = open(configgame,"w+")
-        f.write("<FINAL FANTASY XIV Config File>\n\n<Cutscene Settings>\nCutsceneMovieOpening 1")
-        f.close
-    configpath = os.path.join(util.protonprefix(), 'drive_c/users/steamuser/Documents/My Games/FINAL FANTASY XIV - A Realm Reborn')
-    if not os.path.exists(configpath):
-        os.makedirs(configpath)
-    configgame = os.path.join(configpath, 'FFXIV_BOOT.cfg')
-    if not os.path.isfile(configgame):
-        f = open(configgame,"w+")
-        f.write("<FINAL FANTASY XIV Boot Config File>\n\n<Version>\nBrowser 1\nStartupCompleted 1")
-        f.close
     # Fixes the startup process.
     if 'NOSTEAM' in os.environ:
         util.replace_command('-issteam', '')
