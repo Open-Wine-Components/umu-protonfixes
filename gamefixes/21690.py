08d4391749d2fbb5d5a98ecd198e3710dc1b1d42
Manuel Iglesias García <55031066+manueliglesiasgarcia@users.noreply.github.com>
2022-01-29 14:17:38 +0000

    Update 21690.py
    This is in order to fix a problem with RE5 where videos even though wmp11 and mfplat are working with RE series RE5 seems to not work

--- gamefixes/21690.py
+++ gamefixes/21690.py
@@ -2,9 +2,20 @@
 """
 #pylint: disable=C0103
 
+import os
+import shutil
+import subprocess
 from protonfixes import util
 
 def main():
     """ Installs wmp11
     """
     util.protontricks('wmp11')
+
+    installpath = os.path.abspath(os.getcwd())
+    videopath =  os.path.join(installpath,'nativePC_MT','movie')
+
+    for video in os.listdir(videopath):
+        if video.endswith(".wmv") and os.path.getsize(os.path.join(videopath, video)) > 0:
+            shutil.move(os.path.join(videopath, video), os.path.join(videopath, video + '.bak'))
+            subprocess.call(['touch', os.path.join(videopath, video), os.path.join(videopath)])
