0bb9e19602792dcd0579aa56c5ef9bf071fba91c
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2022-02-16 19:01:35 -0700

    294700: i'm tired, fix symlink

--- gamefixes/294700.py
+++ gamefixes/294700.py
@@ -10,7 +10,7 @@ import os
 def main():
     dosdevice = os.path.join(util.protonprefix(), 'dosdevices/r:')
     if not os.path.exists(dosdevice):
-        os.symlink('/mnt', dosdevice) #create symlink for dosdevices
+        os.symlink('/tmp', dosdevice) #create symlink for dosdevices
 
     util.regedit_add("HKLM\\System\\MountedDevices",'\\??\\Volume{00000000-0000-0000-0000-000000000052}','REG_BINARY','2f746d7000') #sets up ID? exported from regedit
     util.regedit_add("HKLM\\System\\MountedDevices",'\\DosDevices\\R:','REG_BINARY','5c005c002e005c0064003a000000') #sets up dosdevice? exported from regedit
