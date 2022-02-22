6f9354686142b9d79689fa421a05c3cb6be36b48
John Obscurant <usul@gmx.li>
2022-02-22 11:49:31 +0100

    Add protonfix for Civilization IV: Colonization

---
+++ gamefixes/16810.py
@@ -0,0 +1,11 @@
+""" Civilization IV: Colonization
+"""
+
+from protonfixes import util
+
+def main():
+
+    util.protontricks('oleaut32')
+    util.protontricks('msxml3')
+    util.protontricks('corefonts')
+
