a7e1e4e2f7a9e37006872e6bdd47df2b80613ef9
Thomas Crider <gloriouseggroll@gmail.com>
2022-06-22 21:07:42 -0600

    add core count fix for farcry 4

---
+++ gamefixes/298110.py
@@ -0,0 +1,16 @@
+""" FarCry 4
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+import multiprocessing
+
+
+def main():
+    """ FarCry 4 chokes on more than 24 cores
+    """
+
+    util.protontricks('d3dcompiler_43')
+    util.protontricks('d3dcompiler_47')
+    if multiprocessing.cpu_count() > 24:
+        util.set_environment('WINE_CPU_TOPOLOGY', '25:0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24')
