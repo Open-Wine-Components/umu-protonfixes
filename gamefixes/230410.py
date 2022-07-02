89deddc246ec52ee166ddc674b13acc043c69975
Thomas Crider <gloriouseggroll@gmail.com>
2022-07-02 15:00:52 -0600

    add launcher black screen workaround for warframe

--- gamefixes/230410.py
+++ gamefixes/230410.py
@@ -9,3 +9,4 @@ def main():
 
     # gpu acelleration on wibed3d https://bugs.winehq.org/show_bug.cgi?id=44985
     util.winedll_override('libglesv2', 'd')
+    util.append_argument('--use-gl=osmesa')
