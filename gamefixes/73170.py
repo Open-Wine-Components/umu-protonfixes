604143d935c511e34727aa168d914d8beb530cf1
Radu Avramescu <contact@raduavramescu.com>
2022-07-05 21:35:05 +0300

    Added Darkest Hour: A Hearts of Iron Game fix

---
+++ gamefixes/73170.py
@@ -0,0 +1,11 @@
+""" Game fix for Darkest Hour: A Hearts of Iron Game
+"""
+
+from protonfixes import util
+
+def main():
+    """ Set virtual desktop
+    """
+
+    # https://github.com/ValveSoftware/Proton/issues/3338
+    util.protontricks('vd=1280x720')
