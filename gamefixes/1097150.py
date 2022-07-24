1690a59516bd8b03bfa2305c2eda1780ccd9ee76
Thomas Crider <gloriouseggroll@gmail.com>
2022-07-24 15:22:24 -0600

    update fall guys fix to not hang after EOS install on new prefix

--- gamefixes/1097150.py
+++ gamefixes/1097150.py
@@ -10,11 +10,13 @@ def main():
     """
     util.install_eac_runtime()
     util.disable_esync()
-    util.disable_fsync()    
+    util.disable_fsync()
     if os.path.exists('FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'):
         subprocess.call(['rm', '-rf', 'FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'])
     subprocess.call(['ln', '-s', '../../../EasyAntiCheat/easyanticheat_x64.so', 'FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'])
 
-    # Fixes the ini file.
-    subprocess.call(['sed', '-i', 's/TargetApplicationPath=FallGuysEACLauncher.exe/TargetApplicationPath=FallGuys_client_game.exe/', 'FallGuys_client.ini'])
-    
+    #Need to replace the command instead of the ini otherwise it will hang after installing EOS on new prefix
+    util.replace_command('FallGuysEACLauncher.exe', 'FallGuys_client_game.exe')
+    # Fixes the ini files.
+    #subprocess.call(['sed', '-i', 's/TargetApplicationPath=FallGuysEACLauncher.exe/TargetApplicationPath=FallGuys_client_game.exe/', 'FallGuys_client.ini'])
+
