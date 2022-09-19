8ab3242445e5df7bdbab6684c78365a93250a667
Oro <93224879+orowith2os@users.noreply.github.com>
2022-09-19 05:08:15 -0500

    Adds the Shatterline protontricks

---
+++ gamefixes/2087030.py
@@ -0,0 +1,11 @@
+
+""" Shatterline needs a few fixes to be playable: https://www.reddit.com/r/linux_gaming/comments/xhs8e0/shatterline_has_enabled_eac_support_for_proton/
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+
+    util.protontricks('d3dcompiler_47')
+    util.protontricks('win7')
