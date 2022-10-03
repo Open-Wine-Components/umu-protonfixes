430470766185e0dc8f5687fdf528b983cc9d2c81
Oro <93224879+orowith2os@users.noreply.github.com>
2022-10-03 05:43:03 -0500

    adds a note in the Reach protonfix

--- gamefixes/1695793.py
+++ gamefixes/1695793.py
@@ -1,5 +1,6 @@
 """ Game fix for the Halo Reach mod tools Foundation gamemode. 
 Standalone and Sapien seem to work just fine without d3dcompiler_47 and msxml3, although might be required at some deeper level. I just playtested it.
+- Oro, @orowith2os
 """
 #pylint: disable=C0103
 
