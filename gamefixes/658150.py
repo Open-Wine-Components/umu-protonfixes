7de6c437e2812d594ba841989feadb3e32279890
xperia64 <xperiancedapps@gmail.com>
2022-11-14 23:06:28 -0500

    Fix actually going in game in Skeleton Boomerang

---
+++ gamefixes/658150.py
@@ -0,0 +1,12 @@
+""" Skeleton Boomerang
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+
+def main():
+    """ Fixes shader compilation error
+    """
+
+    util.protontricks('d3dcompiler_43')
