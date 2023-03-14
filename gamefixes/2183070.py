fe430fcdf8c4575b37896e5abb18177091f38274
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2023-03-14 13:35:59 -0700

    Add fix to prevent game from hanging
    Currently, disabling ESYNC and FSYNC fixes the game from hanging after proceeding the in-game terminal menu by entering or clicking `search` . 
    
    Additionally, the problem of ESYNC and FSYNC hanging appears to be present in other games using Nitro Plus' game engine such as Song of Saya (702050) and YOU and ME and HER: A Love Story (1293820). Up until now, those problems were never addressed upstream so these workarounds are necessary for the moment.
    
    https://www.protondb.com/app/1293820
    https://www.protondb.com/app/702050

--- gamefixes/2183070.py
+++ gamefixes/2183070.py
@@ -4,8 +4,11 @@
 from protonfixes import util
 
 def main():
-    """ installs xact
+    """ installs xact, disable ESYNC, disable FSYNC
     """
 
-    # Fixes launch after typing then entering `search` within the game's terminal menu
+    # Fixes crash after typing then entering or clicking `search` within the game's terminal menu
     util.protontricks('xact')
+    # Fixes hanging after typing then entering or clicking `search` within the game's terminal menu
+    util.set_environment('PROTON_NO_ESYNC', '1')
+    util.set_environment('PROTON_NO_FSYNC', '1')
