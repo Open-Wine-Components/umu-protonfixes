b541689f90b7278776e3668bfd7d86a15c67cb25
marianoag <131562345+marianoag@users.noreply.github.com>
2023-05-13 02:42:05 -0300

    Update 968370.py

--- gamefixes/968370.py
+++ gamefixes/968370.py
@@ -7,7 +7,4 @@ from protonfixes import util
 
 def main():
     util.set_environment('WINEDLLOVERRIDES', 'd3d9=d')
-    #util.append_argument('/NOF')
-    #util.set_environment('PROTON_USE_WINED3D9', '1')
-    #util.disable_dxvk()
-    util.protontricks('segoe_script5')
+    util.protontricks('segoe_script')
