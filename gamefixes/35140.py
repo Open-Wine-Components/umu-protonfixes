dae6840e78ea8a66beea6b7c60b82e8842d1d55b
george <george@fozzie.dev>
2023-09-30 20:36:46 +0100

    Add physx protontricks to Batman Arkham Asylum

--- gamefixes/35140.py
+++ gamefixes/35140.py
@@ -11,6 +11,7 @@ def main():
     #Probably not needed when proton will be merged with newer wine
     util.protontricks('d3dcompiler_43')
     util.protontricks('d3dx9_43')
+    util.protontricks('physx')
     util._mk_syswow64() #pylint: disable=protected-access
 
 #TODO Controllers fixes
