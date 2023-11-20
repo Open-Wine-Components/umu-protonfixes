64c2b869d9d077589c05ddab8bd6b5d0250015e1
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2023-11-21 00:17:46 +0200

    Restore sound in The Great Ace Attorney Chronicles bonus videos
    This similar fix has been done to many other (Japanese) games using
    Windows Media Foundation.
    
    Credit to Swish, marianoag, Bitwolfies and salixor for creating similar
    fixes previously.

---
+++ gamefixes/1158850.py
@@ -0,0 +1,12 @@
+""" The Great Ace Attorney Chronicles
+Missing sound in bonus content videos
+Requires disabling the gstreamer protonaudioconverterbin to get full audio
+"""
+
+# pylint: disable=C0103
+
+from protonfixes import util
+
+
+def main():
+    util.set_environment("GST_PLUGIN_FEATURE_RANK", "protonaudioconverterbin:NONE")
