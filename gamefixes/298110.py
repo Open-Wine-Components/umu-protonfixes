a3c5187d7e5e1a081043895c3261fcbc2abd12f4
Fabian Arndt <fabian.arndt@root-core.net>
2024-01-08 01:23:35 +0100

    Refactored fixes that use WINE_CPU_TOPOLOGY Moved corresponding logic of gamefixes to util.py
    util.py:
    - Added functions to set the cpu topology
    - Added is_smt_enabled()
    - Added get_cpu_count()

--- gamefixes/298110.py
+++ gamefixes/298110.py
@@ -3,7 +3,6 @@
 #pylint: disable=C0103
 
 from protonfixes import util
-import multiprocessing
 
 
 def main():
@@ -12,5 +11,4 @@ def main():
 
     util.protontricks('d3dcompiler_43')
     util.protontricks('d3dcompiler_47')
-    if multiprocessing.cpu_count() > 24:
-        util.set_environment('WINE_CPU_TOPOLOGY', '25:0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24')
+    util.set_cpu_topology_limit(24)
--- gamefixes/35130.py
+++ gamefixes/35130.py
@@ -3,13 +3,10 @@
 #pylint: disable=C0103
 
 from protonfixes import util
-import multiprocessing
 
 
 def main():
     """ LCGoL chokes on more than 12 cores
     """
 
-    if multiprocessing.cpu_count() > 24:
-        util.set_environment('WINE_CPU_TOPOLOGY', '12:0,1,2,3,4,5,6,7,8,9,10,11')
-
+    util.set_cpu_topology_limit(12)
