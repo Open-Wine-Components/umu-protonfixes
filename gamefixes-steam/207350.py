1ec8ac0bb3c586ff9a52f34e493c1e7a3058113f
GloriousEggroll <gloriouseggroll@gmail.com>
2024-01-21 16:59:10 -0700

    whoops, revert changes to ys origin (was testing something)

--- gamefixes-steam/207350.py
+++ gamefixes-steam/207350.py
@@ -8,9 +8,4 @@ def main():
     """ installs k-lite
     """
 
-    util.protontricks('vcrun2008')
-    util.protontricks('quartz')
-    util.protontricks('amstream')
-    util.protontricks('xvid')
-    util.protontricks('win7')
-    util.disable_protonaudioconverter()
+    util.protontricks('klite')
