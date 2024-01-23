cc8b7cad69be0271a5168661260d782f4b64273d
GloriousEggroll <gloriouseggroll@gmail.com>
2024-01-22 18:43:11 -0700

    catherine classic videos work now!

--- gamefixes-Steam/893180.py
+++ gamefixes-Steam/893180.py
@@ -7,5 +7,10 @@ from protonfixes import util
 def main():
     """
     """
+    util.protontricks('directshow')
+    util.protontricks('cinepak')
+    util.protontricks('lavfilters')
     util.protontricks('d3dcompiler_43')
     util.protontricks('d3dcompiler_47')
+    util.disable_protonaudioconverter()
+    util.winedll_override('winegstreamer', '')
