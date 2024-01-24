9b7df256efba81820a449b3b86edd7e4f16b9bbe
Paweł Lidwin <lidwinpawel@gmail.com>
2024-01-24 18:44:33 +0100

    set SteamGameId for Horizon

--- gamefixes-GOG/ulwgl-1151640.py
+++ gamefixes-GOG/ulwgl-1151640.py
@@ -10,3 +10,5 @@ def main():
 
     # C++ runtime is not provided in the manifest
     util.protontricks('vcrun2019')
+    # Set SteamGameId so that non-steam versions can pick up steam-specific fixes in proton's wine code
+    util.set_environment('SteamGameId','1151640')
