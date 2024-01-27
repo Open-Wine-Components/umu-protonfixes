4805a78ba2b29b1b0335ec889eb96eeee725df1e
GloriousEggroll <gloriouseggroll@gmail.com>
2024-01-27 03:39:31 -0700

    add symlink fixup script in case of bad capitalization and/or removal of fixes symlinked to other fixes, resolves #1

--- symlinkfix.sh
+++ symlinkfix.sh
@@ -5,3 +5,17 @@ find . -type l | while read symlink; do
   new_target=$(echo "$target" | tr '[:upper:]' '[:lower:]')
   ln -sf "$new_target" "$symlink"
 done
+
+# iterate through game folders, check if symlinks are valid, if they are not, remove them
+
+for gamefixes in $(ls | grep gamefixes); do
+	cd $gamefixes
+	find . -type l | while read symlink; do
+  		target=$(readlink "$symlink")
+ 		if [[ ! -e "$target" ]]; then
+			echo "Broken symlink detected, removing: $gamefixes/$(echo "$symlink" | sed 's/^..//')"
+    			rm "$symlink"
+ 		fi
+	done
+	cd ..
+done
