7bd6d5ff554bda35910f68f83885633d1fa89e1b
Bitwolf <bitwolf@wolfware.ca>
2024-01-28 11:43:03 -0800

    Game fix for Re:ZERO -Starting Life in Another World- The Prophecy of the Throne
    Solves https://github.com/Open-Wine-Components/ULWGL-protonfixes/issues/3 supposedly, don't own the game myself however.

---
+++ gamefixes-steam/1277510.py
@@ -0,0 +1,10 @@
+""" Game fix for Re:ZERO -Starting Life in Another World- The Prophecy of the Throne
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """
+    """
+    util.protontricks('d3dcompiler_47')
