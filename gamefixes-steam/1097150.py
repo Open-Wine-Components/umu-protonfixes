e56140ba7460dada95445100a518ae8812b9c524
GloriousEggroll <gloriouseggroll@gmail.com>
2024-01-29 01:23:54 -0700

    add fix that allows fall guys steam version to work again

--- gamefixes-steam/1097150.py
+++ gamefixes-steam/1097150.py
@@ -1,12 +1,13 @@
 """ Game fix for Fall Guys
 """
 #pylint: disable=C0103
+import os
+import glob
+import subprocess
 from protonfixes import util
 
 def main():
     """ Create symlink of eac so at the right location
     """
     util.install_eac_runtime()
-    util.disable_esync()
-    util.disable_fsync()
-
+    util.set_environment('DOTNET_BUNDLE_EXTRACT_BASE_DIR', '')
