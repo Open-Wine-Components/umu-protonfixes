cdd3aaea3cce56cfda168d189b90f9c5ac53a552
Paweł Lidwin <lidwinpawel@gmail.com>
2024-02-03 16:38:53 +0100

    add dinput override to MGS
    latest build ships it now, additonally added builtin fallbacks if GOG were to push more changes to the build

--- gamefixes-gog/ulwgl-metalgearsolid.py
+++ gamefixes-gog/ulwgl-metalgearsolid.py
@@ -10,4 +10,5 @@ def main():
     override for wrapper shipped with the game
     """
 
-    util.winedll_override('ddraw', 'n')
+    util.winedll_override('ddraw', 'n,b')
+    util.winedll_override('dinput', 'n,b')
