de8869138773a01c572ecbf6e1a3e94842d102e4
GloriousEggroll <gloriouseggroll@gmail.com>
2024-02-04 00:45:08 -0700

    add ulwgl entry for black desert online, make sure envvars are set

--- gamefixes-steam/582660.py
+++ gamefixes-steam/582660.py
@@ -11,3 +11,7 @@ def main():
     # Fixes the startup process.
     if 'NOSTEAM' in os.environ:
         util.replace_command('--steam', '')
+    # Needed for settings archive
+    util.set_environment('SteamGameId','582660')
+    # Needed for Launcher
+    util.set_environment('WINE_DISABLE_KERNEL_WRITEWATCH','1')
---
+++ gamefixes-ulwgl/ulwgl-582660.py
@@ -0,0 +1,1 @@
+../gamefixes-steam/582660.py
