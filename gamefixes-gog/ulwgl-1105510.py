f2e629422e640758734d142a2b93cfb37b715d4c
GloriousEggroll <gloriouseggroll@gmail.com>
2024-02-03 18:40:38 -0700

    add entries for yakuza 5 remastered

---
+++ gamefixes-gog/ulwgl-1105510.py
@@ -0,0 +1,1 @@
+../gamefixes-steam/1105510.py
---
+++ gamefixes-steam/1105510.py
@@ -0,0 +1,12 @@
+""" Game fix for Yakuza 5
+"""
+#pylint: disable=C0103
+import os
+from protonfixes import util
+
+def main():
+    """ Needs WINE_DISABLE_SFN set from this patch: https://github.com/ValveSoftware/wine/pull/205
+    """
+
+    util.set_environment('WINE_DISABLE_SFN', '1')
+
