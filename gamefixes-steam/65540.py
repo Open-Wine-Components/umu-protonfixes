29557b0440a922259665d5a633b282348a6df213
Paweł Lidwin <lidwinpawel@gmail.com>
2024-02-18 13:03:44 +0100

    gothic 1 - use path relative to install directory

--- gamefixes-steam/65540.py
+++ gamefixes-steam/65540.py
@@ -25,7 +25,7 @@ def main():
     zVidResFullscreenBPP=32
     """
 
-    util.set_ini_options(game_opts,'Gothic.ini','cp1251','game')
+    util.set_ini_options(game_opts,'system/Gothic.ini','cp1251','game')
 
     # Fix the game getting locked on exit
     util.disable_fsync()
