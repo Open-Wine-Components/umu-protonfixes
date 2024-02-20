d383cc68927fcd2f744aed18d20952db295d50e9
Keenan Weaver <keenanweaver@protonmail.com>
2024-02-20 14:57:45 -0600

    gog: Add Soldier of Fortune II

---
+++ gamefixes-gog/ulwgl-1228964594.py
@@ -0,0 +1,12 @@
+""" Game fix for Soldier of Fortune II: Double Helix - Gold Edition 
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ """
+
+    # Fix display issues
+    util.set_environment('MESA_EXTENSION_MAX_YEAR', '2003')
+    util.set_environment('__GL_ExtensionStringVersion', '17700')
