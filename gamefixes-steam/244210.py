f950612961135efadeac9f7714ae5b41c0e4f15b
GloriousEggroll <gloriouseggroll@gmail.com>
2024-03-04 01:47:56 -0700

    fixup assetto corsa protonfixes to allow game to run again

--- gamefixes-steam/244210.py
+++ gamefixes-steam/244210.py
@@ -7,10 +7,10 @@ from protonfixes import util
 def main():
     """Fixes default launcher and ACM."""
 
-    # Fixes Assetto itself and Content Manager. Version 4.5.2 does not seem to start.
-    # Although CefSharp from Content Manager might have a higher requirement, it seems to
-    # always crash on the GPU process so it doesn't really matter.
-    util.protontricks('dotnet472')
+    util.protontricks('dotnet452')
     # Fixes Content Manager (black windows)
     util.protontricks('d3dx11_43')
     util.protontricks('d3dcompiler_47')
+    util.winedll_override('dwrite', 'n,b')
+    util.protontricks('win10')
+    util.set_environment('PULSE_LATENCY_MSEC', '60')
