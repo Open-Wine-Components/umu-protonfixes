69256ca755e8e2120235d701acc4e1287fc37367
Aqa-Ib <victorcana@gmx.com>
2024-03-05 19:30:36 +0100

    fix comments

--- gamefixes-steam/219990.py
+++ gamefixes-steam/219990.py
@@ -6,14 +6,9 @@ from protonfixes import util
 import os
 
 def main():
-    """ Run script extender if it exists.
-    """
-
-    # Fixes the startup process.
+    # Run script extender if it exists:
     if os.path.isfile(os.path.join(os.getcwd(), 'GrimInternals64.exe')):
         util.replace_command('Grim Dawn.exe', 'GrimInternals64.exe')
 
-    
-    """ Fix black screen """
-    # Needed for the expansions, not for the base game.
+    # Fix black screen. Needed for the expansions, not for the base game:
     util.protontricks('d3dcompiler_43')
