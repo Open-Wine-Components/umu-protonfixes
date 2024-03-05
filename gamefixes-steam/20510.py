73bb3848dbb103ee1659d9d97c85a0f58cf3fd29
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2024-03-05 17:39:44 +0900

    add fix descriptors for S.T.A.L.K.E.R.: Clear Sky

--- gamefixes-steam/20510.py
+++ gamefixes-steam/20510.py
@@ -8,8 +8,10 @@ def main():
     """ installs d3dcomp43, d3dcomp46, d3dcomp47, directplay
     """
 
-    # Discord report in #proton-gaming from @thebigboo_
+    # from Discord report in #proton-gaming by @thebigboo_ 
+    # fix crashing from d3d comp errors
     util.protontricks('d3dcompiler_43')
     util.protontricks('d3dcompiler_46')
     util.protontricks('d3dcompiler_47')
+    # fixes multiplayer
     util.protontricks('directplay')
