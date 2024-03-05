e8db74183fef43c033a9ccab39c35458de27d9f8
Aqa-Ib <victorcana@gmx.com>
2024-03-05 21:29:38 +0100

    Clarify Grim Dawn Fix (#31)
    Explain grim dawn fix

--- gamefixes-steam/219990.py
+++ gamefixes-steam/219990.py
@@ -3,16 +3,12 @@
 #pylint: disable=C0103
 
 from protonfixes import util
-import subprocess
 import os
 
 def main():
-    """ Run script extender if it exists.
-    """
-
-    # Fixes the startup process.
+    # Run script extender if it exists:
     if os.path.isfile(os.path.join(os.getcwd(), 'GrimInternals64.exe')):
         util.replace_command('Grim Dawn.exe', 'GrimInternals64.exe')
 
-    # Fixes a black screen being rendered:
+    # Fix black screen. Needed for the expansions, not for the base game:
     util.protontricks('d3dcompiler_43')
