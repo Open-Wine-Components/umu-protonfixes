0409c35e6cfbaed3f060063411344c34bb5630e0
Aqa-Ib <victorcana@gmx.com>
2024-03-06 22:00:04 +0100

    Grim Dawn: remove unsupported mod (#33)
    Grim Dawn: remove deprecated mod

--- gamefixes-steam/219990.py
+++ gamefixes-steam/219990.py
@@ -3,12 +3,7 @@
 #pylint: disable=C0103
 
 from protonfixes import util
-import os
 
 def main():
-    # Run script extender if it exists:
-    if os.path.isfile(os.path.join(os.getcwd(), 'GrimInternals64.exe')):
-        util.replace_command('Grim Dawn.exe', 'GrimInternals64.exe')
-
     # Fix black screen. Needed for the expansions, not for the base game:
     util.protontricks('d3dcompiler_43')
