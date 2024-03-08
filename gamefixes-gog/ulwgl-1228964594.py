66fd92f4ab8f9d10acf98c10694f9e587c661b5a
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-03-08 14:37:55 -0800

    Lint GOG gamefixes

--- gamefixes-gog/ulwgl-1228964594.py
+++ gamefixes-gog/ulwgl-1228964594.py
@@ -5,8 +5,6 @@
 from protonfixes import util
 
 def main():
-    """ """
-
     # Fix display issues
     util.set_environment('MESA_EXTENSION_MAX_YEAR', '2003')
     util.set_environment('__GL_ExtensionStringVersion', '17700')
