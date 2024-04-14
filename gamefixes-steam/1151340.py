a1309e34769f09fa898679ac1c2426a4e8340284
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-04-14 09:28:41 -0700

    Revert "Game fix for Fallout 76"
    This reverts commit 38bce313b9814325ee44bca9f39a6f816a1ca0e9.

--- gamefixes-steam/1151340.py
+++
@@ -1,9 +0,0 @@
-""" Game fix for Fallout 76
-"""
-#pylint: disable=C0103
-from protonfixes import util
-
-def main():
-    """ Ensure Faudio is installed so UI and NPC audio and music is audible
-    """
-    util.protontricks('faudio')
