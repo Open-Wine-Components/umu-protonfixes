7939ee36efdcd203f5d43d024d6b722901157747
Creeper of Doom <34628285+CreeperofDoom@users.noreply.github.com>
2024-04-15 17:01:53 -0400

    Add files via upload

---
+++ gamefixes-steam/1829980.py
@@ -0,0 +1,15 @@
+""" Game fix for Cafe Stella
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """
+    """
+    util.protontricks('quartz')
+    util.protontricks('wmp9')
+    util.protontricks('wmp9_x86_64')
+    util.protontricks('klite')
+    util.protontricks('directshow')
+    util.disable_protonaudioconverter()
