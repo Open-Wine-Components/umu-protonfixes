4cc1e05f7279a954a5d3f0e5473919a14a0ae4fc
Creeper of Doom <34628285+CreeperofDoom@users.noreply.github.com>
2024-04-15 20:41:23 -0400

    Add files via upload

---
+++ gamefixes-steam/1277930.py
@@ -0,0 +1,13 @@
+""" Game fix for Riddle Joker
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """
+    """
+    util.protontricks('quartz')
+    util.protontricks('wmp9')
+    util.protontricks('directshow')
+    util.disable_protonaudioconverter()
