272b4f92bbbffac6b7970ac912c246455e6b714c
Creeper of Doom <34628285+CreeperofDoom@users.noreply.github.com>
2024-04-16 09:50:38 -0400

    Add files via upload

---
+++ gamefixes-steam/1144400.py
@@ -0,0 +1,11 @@
+""" Game fix for Senren Banka
+"""
+#pylint: disable=C0103
+from protonfixes import util
+def main():
+    """
+    """
+    util.protontricks('quartz')
+    util.protontricks('wmp9')
+    util.protontricks('directshow')
+    util.disable_protonaudioconverter()
