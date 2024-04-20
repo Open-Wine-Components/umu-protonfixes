e5ae22679bd5a277f58f353ed570051d22c238b7
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-04-19 17:46:47 -0700

    Add tests for get_game_name

--- protonfixes_test.py
+++ protonfixes_test.py
@@ -3,6 +3,9 @@ import unittest
 import os
 import tempfile
 from pathlib import Path
+from unittest.mock import patch
+import io
+import urllib.request
 import fix
 
 # pylint: disable=C0115,R0904
@@ -13,7 +16,8 @@ class TestProtonfixes(unittest.TestCase):
             'SteamAppId': '',
             'SteamGameId': '',
             'STEAM_COMPAT_DATA_PATH': '',
-            'UMU_ID': ''
+            'UMU_ID': '',
+            'DEBUG': ''
         }
         self.game_id = '1293820'
         self.pfx = Path(tempfile.mkdtemp())
@@ -23,6 +27,9 @@ class TestProtonfixes(unittest.TestCase):
             if key in os.environ:
                 os.environ.pop(key)
         if self.pfx.is_dir():
+            if self.pfx.joinpath('steamapps').is_dir():
+                self.pfx.joinpath('steamapps', 'appmanifest_1628350.acf').unlink(missing_ok=True)
+                self.pfx.joinpath('steamapps').rmdir()
             self.pfx.joinpath('game_title').unlink(missing_ok=True)
             self.pfx.rmdir()
 
@@ -280,6 +287,115 @@ class TestProtonfixes(unittest.TestCase):
         self.pfx.joinpath('game_title').touch()
         result = fix.get_game_name()
         self.assertFalse(result, 'Expected an empty string')
+
+    def testGetGameNameDB(self):
+        """Set UMU_ID and access umu database
+        
+        Makes a request to the umu database for a title name to be displayed
+        if a UMU_ID is supplied. This function should be fail safe in case of
+        a TimeoutError, OSError, IndexError or UnicodeDecodeError
+        """
+        os.environ['UMU_ID'] = 'umu-35140'
+        os.environ['WINEPREFIX'] = self.pfx.as_posix()
+        # Mock a valid umu db response
+        data = '[{"title":"Batman: Arkham Asylum Game of the Year Edition","umu_id":"umu-35140","acronym":null,"codename":"1482504285","store":"gog","notes":null},{"title":"Batman: Arkham Asylum Game of the Year Edition","umu_id":"umu-35140","acronym":null,"codename":"godwit","store":"egs","notes":null}]'
+        data = io.StringIO(data)
+        with (
+            patch.object(fix, 'check_internet', return_value=True),
+            patch.object(urllib.request, 'urlopen', return_value=data)
+        ):
+            func = fix.get_game_name.__wrapped__  # Do not reference the cache
+            result = func()
+            self.assertEqual(result, 'Batman: Arkham Asylum Game of the Year Edition')
+
+    def testGetGameNameDBTimeout(self):
+        """Set UMU_ID and access umu database
+        
+        Mock the TimeoutError
+        """
+        os.environ['UMU_ID'] = 'umu-35140'
+        os.environ['WINEPREFIX'] = self.pfx.as_posix()
+        # Mock a valid umu db response
+        with (
+            patch.object(fix, 'check_internet', return_value=True),
+            patch.object(urllib.request, 'urlopen') as mock_function
+        ):
+            mock_function.side_effect = TimeoutError
+            func = fix.get_game_name.__wrapped__  # Do not reference the cache
+            result = func()
+            self.assertEqual(result, 'UNKNOWN')
+
+    def testGetGameNameDbOS(self):
+        """Set UMU_ID and access umu database
+        
+        Mock the OSError, which only shown if debugging is enabled
+        """
+        os.environ['UMU_ID'] = 'umu-35140'
+        os.environ['WINEPREFIX'] = self.pfx.as_posix()
+        os.environ['DEBUG'] = '1'
+        # Mock a valid umu db response
+        with (
+            patch.object(fix, 'check_internet', return_value=True),
+            patch.object(urllib.request, 'urlopen') as mock_function
+        ):
+            mock_function.side_effect = OSError
+            func = fix.get_game_name.__wrapped__  # Do not reference the cache
+            result = func()
+            self.assertEqual(result, 'UNKNOWN')
+
+    def testGetGameNameDbIndex(self):
+        """Set UMU_ID and access umu database
+        
+        Mock the IndexError
+        """
+        os.environ['UMU_ID'] = 'umu-35140'
+        os.environ['WINEPREFIX'] = self.pfx.as_posix()
+        os.environ['DEBUG'] = '1'
+        # Mock a valid umu db response
+        with (
+            patch.object(fix, 'check_internet', return_value=True),
+            patch.object(urllib.request, 'urlopen') as mock_function
+        ):
+            mock_function.side_effect = IndexError
+            func = fix.get_game_name.__wrapped__  # Do not reference the cache
+            result = func()
+            self.assertEqual(result, 'UNKNOWN')
+
+    def testGetGameNameDbUnicode(self):
+        """Set UMU_ID and access umu database
+        
+        Mock the UnicodeError
+        """
+        os.environ['UMU_ID'] = 'umu-35140'
+        os.environ['WINEPREFIX'] = self.pfx.as_posix()
+        os.environ['DEBUG'] = '1'
+        def mock_urlopen_raise_error(*args, **kwargs):
+            raise UnicodeDecodeError('utf-8', b'', 0, 1, '')
+        # Mock a valid umu db response
+        with (
+            patch.object(fix, 'check_internet', return_value=True),
+            patch.object(urllib.request, 'urlopen') as mock_function
+        ):
+            mock_function.side_effect = mock_urlopen_raise_error
+            func = fix.get_game_name.__wrapped__  # Do not reference the cache
+            result = func()
+            self.assertEqual(result, 'UNKNOWN')
+
+    def testGetGameNameNoManifest(self):
+        """Do not set UMU_ID and try to get the title from the steam app
+        library
+
+        UNKNOWN should be returned because no manifest file will exist in the
+        test directory
+        """
+        os.environ['SteamAppId'] = '1628350'
+        os.environ['WINEPREFIX'] = self.pfx.as_posix()
+        os.environ['PWD'] = os.environ['WINEPREFIX']
+        steamapps = self.pfx.joinpath('steamapps')
+        steamapps.mkdir()
+        func = fix.get_game_name.__wrapped__  # Do not reference the cache
+        result = func()
+        self.assertEqual(result, 'UNKNOWN')
 
 if __name__ == '__main__':
     unittest.main()
