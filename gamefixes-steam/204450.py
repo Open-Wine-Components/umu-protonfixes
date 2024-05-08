d3828b20c9574913674bd898f8c4c66826c6c842
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2024-05-08 15:16:33 -0300

    Adjust comments

--- gamefixes-steam/204450.py
+++ gamefixes-steam/204450.py
@@ -1,4 +1,4 @@
-""" Game fix Call of Juarez: Gunslinger
+""" Game fixes Call of Juarez: Gunslinger
 """
 #pylint: disable=C0103
 
@@ -7,7 +7,7 @@ from protonfixes import util
 def main():
     """ installs wmp11
     """
-    # Fixes missing audio cutscenes
+    # Fixes missing cutscenes
     util.protontricks('wmp11')
     # Seems to choke on more than 32 cores, needs testing
     util.set_cpu_topology_limit(31)
