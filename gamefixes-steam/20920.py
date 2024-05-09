d0a3905f1b11be5fea66af9ed3f3b9c10f52f1d2
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-05-08 20:16:05 -0700

    Lint with Pylint

--- gamefixes-steam/20920.py
+++ gamefixes-steam/20920.py
@@ -1,10 +1,5 @@
 """ The Witcher 2: Assassins of Kings Enhanced Edition
 """
-#pylint: disable=C0103
-
-from protonfixes import util
-
-
 def main():
     """ Witcher 2 chokes on more than 16 cores
     Code block is intentionally empty because this game exists in GOG (umu-20920)
