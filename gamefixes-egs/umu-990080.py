14e0d9012d3884235dbf5016e61fccb96e7efbba
Jan-Willem de Bruyn <git@jan-willem.dev>
2024-06-16 18:04:40 +0200

    Moved fix Hogwarts Legacy from steam to egs

--- gamefixes-egs/umu-990080.py
+++ gamefixes-egs/umu-990080.py
@@ -1,1 +1,9 @@
-../gamefixes-steam/990080.py
+""" Game fix Hogwarts Legacy
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    # Requires vcrun2019 to launch
+    util.protontricks('vcrun2019')
--- gamefixes-steam/990080.py
+++
@@ -1,9 +0,0 @@
-""" Game fix Hogwarts Legacy
-"""
-#pylint: disable=C0103
-
-from protonfixes import util
-
-def main():
-    # Requires vcrun2019 to launch
-    util.protontricks('vcrun2019')
