720a452d2cdb742384da9af47b82a44529c08bb4
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-07-02 19:13:59 -0700

    Update Eldren Ring fix
    - Fixes the indentation and refactors to a single line.

--- gamefixes-steam/1245620.py
+++ gamefixes-steam/1245620.py
@@ -3,8 +3,8 @@
 from pathlib import Path
 from protonfixes import util
 
+
 def main():
-	if not Path(f"{util.get_game_install_path()}/Game/DLC.bdt").exists():
-		# Create the DLC.bdt file if it doesn't already exist, which is known to fix Easy AntiCheat not working for players that don't own the DLC
-		# A blank file is enough to get multiplayer working
-		open(f"{util.get_game_install_path()}/Game/DLC.bdt", 'wb').close()
+    # Create the DLC.bdt file if it doesn't already exist, which is known to fix Easy AntiCheat not working for players that don't own the DLC
+    # A blank file is enough to get multiplayer working
+    Path(f"{util.get_game_install_path()}/Game/DLC.bdt").touch(exist_ok=True)
