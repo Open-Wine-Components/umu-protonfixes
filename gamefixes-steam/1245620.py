ad07482c9cb0a017bf4ba4fb2a252e468d3dc5a8
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-07-02 22:08:27 -0400

    Add workaround for Elden Ring multiplayer for players that don't own the DLC (#87)
    * Add Elden Ring DLC.bdt fix
    
    * Fix incorrect path for DLC.bdt
    
    * Update 1245620.py
    
    * Shorten import

---
+++ gamefixes-steam/1245620.py
@@ -0,0 +1,10 @@
+"""Game fix for Elden Ring: Manually create the `DLC.bdt` file to work around the "Inappropriate activity detected" error for players that don't own the DLC"""
+
+from pathlib import Path
+from protonfixes import util
+
+def main():
+	if not Path(f"{util.get_game_install_path()}/Game/DLC.bdt").exists():
+		# Create the DLC.bdt file if it doesn't already exist, which is known to fix Easy AntiCheat not working for players that don't own the DLC
+		# A blank file is enough to get multiplayer working
+		open(f"{util.get_game_install_path()}/Game/DLC.bdt", 'wb').close()
