0db6d8fae71b525503299a8ee04ea65a973238b6
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-07-24 13:40:51 -0700

    Refactor file write of yml

--- util.py
+++ util.py
@@ -466,7 +466,7 @@ def disable_uplay_overlay() -> bool:
     """
     config_dir = os.path.join(
         protonprefix(),
-        'drive_c/users/steamuser/Local Settings/Application Data/Ubisoft Game Launcher/'
+        'drive_c/users/steamuser/Local Settings/Application Data/Ubisoft Game Launcher/',
     )
 
     if not os.path.exists(config_dir):
@@ -475,13 +475,24 @@ def disable_uplay_overlay() -> bool:
     config_file = os.path.join(config_dir, 'settings.yml')
 
     if not os.path.isdir(config_dir):
-        log.warn(f'Could not disable UPlay overlay: "{config_dir}" does not exist or is not a directory.')
+        log.warn(
+            f'Could not disable UPlay overlay: "{config_dir}" does not exist or is not a directory.'
+        )
         return False
 
     try:
+        data = """
+        overlay:
+          enabled: false
+          forceunhookgame: false
+          fps_enabled: false
+          warning_enabled: false
+        user:
+          closebehavior: CloseBehavior_Close
+        """.strip()
+
         with open(config_file, 'a+', encoding='ascii') as file:
-            file.write('\noverlay:\n  enabled: false\n  forceunhookgame: false'
-                       '\n  fps_enabled: false\n  warning_enabled: false\nuser:\n  closebehavior: CloseBehavior_Close\n')
+            file.write(data)
         log.info('Disabled UPlay overlay')
         return True
     except OSError as err:
