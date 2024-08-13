c646ba3000047b6a2ac3b6713ea2190170f6932f
mactan-sc <77816662+mactan-sc@users.noreply.github.com>
2024-08-12 21:11:15 -0500

    star citizen gamefixes

--- gamefixes-umu/umu-starcitizen.py
+++ gamefixes-umu/umu-starcitizen.py
@@ -1,9 +1,9 @@
 """ Game fix for Star Citizen
 """
-#pylint: disable=C0103
+#pylint: disable=C0103,E0401
 
 import os
-from protonfixes import util # pylint: disable=E0401
+from protonfixes import util
 from protonfixes.logger import log
 
 def main():
@@ -19,19 +19,5 @@ def main():
     #needed for amd vulkan
     util.set_environment('dual_color_blend_by_location','true')
 
-    #override for white/black launcher
-    util.winedll_override('libglesv2', 'builtin')
-    #override for nvidia cards
-    util.winedll_override('nvapi,nvapi64', 'disabled')
     #allow the RSI Launcher to auto-update itself
-    util.winedll_override('powershell.exe', 'disabled')
-
-    environments = ["LIVE","PTU","EPTU","TECH-PREVIEW"]
-
-    for env in environments:
-        #launcher fails to create these directories in wine so create them here instead
-        #https://github.com/starcitizen-lug/knowledge-base/wiki#game-updates
-        envPath = os.path.join(util.protonprefix(), "drive_c","Program Files", "Roberts Space Industries", "StarCitizen", env)
-        if not os.path.exists(envPath):
-            os.makedirs(envPath)
-            log("created " + envPath)
+    util.protontricks('powershell')
