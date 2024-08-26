80327b8c98ceec24bec288d0df44d7828310f0e7
Fabian Arndt <fabian.arndt@root-core.net>
2024-08-26 05:36:48 +0200

    EGS: Added fix for "CYGNI: All Guns Blazing"

---
+++ gamefixes-egs/umu-1248080.py
@@ -0,0 +1,8 @@
+""" Game fix for CYGNI: All Guns Blazing
+"""
+
+from protonfixes import util
+
+def main() -> None:
+    # EGS only: This fixes the startup process.
+    util.append_argument('-epicdeploymentid=78a046d4ac1b42d7aaba9fe80f88a5d8')
