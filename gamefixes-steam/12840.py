5cabadb33d177d6d9426185fb2209b06dde0de81
Jack Greiner <jack@emoss.org>
2024-08-26 22:38:37 -0400

    steam: Add Dirt 2 protonfix

---
+++ gamefixes-steam/12840.py
@@ -0,0 +1,11 @@
+""" Game fix for Dirt 2
+"""
+
+from protonfixes import util
+
+
+def main():
+    """ This game uses GFWL, which causes it to fail to launch. xliveless is needed to get this working.
+    """
+
+    util.protontricks('xliveless')
