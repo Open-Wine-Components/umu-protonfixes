456cfe88d31b7c728f6f2ad37d3aee406c2c0f12
Fabian Arndt <fabian.arndt@root-core.net>
2024-08-27 20:38:01 +0200

    Added game fix for Bully: Scholarship Edition

---
+++ gamefixes-steam/12200.py
@@ -0,0 +1,11 @@
+
+""" Game fix for Bully: Scholarship Edition
+"""
+
+from protonfixes import util
+
+def main():
+    """ Video playback is working since GE-Proton 9.11 - without audio
+        Disabling the media converter fixes this issue
+    """
+    util.disable_protonmediaconverter()
