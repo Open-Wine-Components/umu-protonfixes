dd46adb68cd114f46cf117b69930c36ce8748cb4
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-08-29 13:35:41 -0700

    Update fix for Café Stella and the Reaper's Butterflies

--- gamefixes-steam/1829980.py
+++ gamefixes-steam/1829980.py
@@ -1,12 +1,10 @@
-""" Game fix for Café Stella and the Reaper's Butterflies
-"""
+"""Game fix for Café Stella and the Reaper's Butterflies"""
+
 from protonfixes import util
 
-def main():
-    """Install quartz, wmp11, qasf
 
+def main():
+    """
     Fixes in-game video playback for the intro and ending.
     """
-    util.protontricks('quartz')
-    util.protontricks('wmp11')
-    util.protontricks('qasf')
+    util.disable_protonmediaconverter()
