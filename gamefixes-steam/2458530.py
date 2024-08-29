fec1850a07b86e0fb3bb48461d13b221ef23eedb
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-08-29 13:36:00 -0700

    Update fix for Sanoba Witch FHD Edition

--- gamefixes-steam/2458530.py
+++ gamefixes-steam/2458530.py
@@ -1,12 +1,10 @@
-""" Game fix for Sanoba Witch FHD Edition
-"""
+"""Game fix for Sanoba Witch FHD Edition"""
+
 from protonfixes import util
 
-def main():
-    """Install quartz, wmp11, qasf
 
+def main():
+    """
     Fixes in-game video playback for the intro and ending.
     """
-    util.protontricks('quartz')
-    util.protontricks('wmp11')
-    util.protontricks('qasf')
+    util.disable_protonmediaconverter()
