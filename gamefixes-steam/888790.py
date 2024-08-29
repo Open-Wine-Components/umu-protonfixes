f6e1c169f7f81b77f90050512b36b74769713379
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-08-29 13:36:12 -0700

    Update fix for Sabbat of the Witch

--- gamefixes-steam/888790.py
+++ gamefixes-steam/888790.py
@@ -1,12 +1,10 @@
-""" Game fix for Sabbat of the Witch
-"""
+"""Game fix for Sabbat of the Witch"""
+
 from protonfixes import util
 
-def main():
-    """Install quartz, wmp11, qasf
 
+def main():
+    """
     Fixes in-game video playback for the intro and ending.
     """
-    util.protontricks('quartz')
-    util.protontricks('wmp11')
-    util.protontricks('qasf')
+    util.disable_protonmediaconverter()
