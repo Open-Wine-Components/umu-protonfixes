f20767b315979d61bafc49986ad1b39e404f9ff4
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-08-30 20:44:55 -0700

    Revert "Update fix for Sanoba Witch FHD Edition"
    This reverts commit fec1850a07b86e0fb3bb48461d13b221ef23eedb.

--- gamefixes-steam/2458530.py
+++ gamefixes-steam/2458530.py
@@ -1,10 +1,12 @@
-"""Game fix for Sanoba Witch FHD Edition"""
-
+""" Game fix for Sanoba Witch FHD Edition
+"""
 from protonfixes import util
 
-
 def main():
-    """
+    """Install quartz, wmp11, qasf
+
     Fixes in-game video playback for the intro and ending.
     """
-    util.disable_protonmediaconverter()
+    util.protontricks('quartz')
+    util.protontricks('wmp11')
+    util.protontricks('qasf')
