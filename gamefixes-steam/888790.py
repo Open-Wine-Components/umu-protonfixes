835bf7d61739941488bad6112607c67c0ba28d2e
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-08-30 20:42:46 -0700

    Prevent crashes for Sabbat of the Witch

--- gamefixes-steam/888790.py
+++ gamefixes-steam/888790.py
@@ -8,3 +8,6 @@ def main():
     Fixes in-game video playback for the intro and ending.
     """
     util.disable_protonmediaconverter()
+    # Changes the video renderer to 'overlay' to prevent random crashes
+    # See https://github.com/Open-Wine-Components/umu-protonfixes/pull/115#issuecomment-2319197337
+    util.append_argument("-vomstyle=overlay")
