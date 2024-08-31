0bd7e8f0a59cf96edb6a1cac8211cbd167d91cc7
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-08-31 10:38:46 -0700

    Update fix for ATRI -My Dear Moments-
    - Currently works without installing any winetricks verbs. Instead, the proton media converter should be disabled and the engine's video renderer should be configured to 'overlay'. In the SteamOS gamescope session, oddly, it's observed that moving the trackpad seems to be required to get the video to appear.
    
    - Related to https://github.com/Open-Wine-Components/umu-protonfixes/issues/111

--- gamefixes-steam/1230140.py
+++ gamefixes-steam/1230140.py
@@ -1,12 +1,10 @@
-""" Game fix for ATRI -My Dear Moments-
-"""
-#pylint: disable=C0103
+"""Game fix for ATRI -My Dear Moments-"""
 
 from protonfixes import util
 
+
 def main():
-    """ Installs wmp11
-    """
-    util.protontricks('quartz_feb2010')
-    util.protontricks('wmp11')
-    util.protontricks('qasf')
+    # Fix in-game media playback
+    # See https://github.com/Open-Wine-Components/umu-protonfixes/issues/111#issuecomment-2317389123
+    util.disable_protonmediaconverter()
+    util.append_argument("-vomstyle=overlay")
