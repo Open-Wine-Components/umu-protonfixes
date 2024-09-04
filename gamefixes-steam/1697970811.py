dc754baad2819d70e225e763e2a9b8aaf3aeea48
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-09-03 18:29:34 -0700

    gamefixes-steam: ruff format

--- gamefixes-steam/1697970811.py
+++ gamefixes-steam/1697970811.py
@@ -14,34 +14,34 @@ from protonfixes import util
 from protonfixes.logger import log
 
 
-def main():
+def main() -> None:
     env = protonmain.g_session.env.copy()
-    wine = f"{util.protondir()}/files/bin/wine64"
+    wine = f'{util.protondir()}/files/bin/wine64'
     install_dir = util.get_game_install_path()
 
     # Font installer inside the `fonts` subdir
-    font_installer = "overlock_mod_font_installer.exe"
+    font_installer = 'overlock_mod_font_installer.exe'
 
     # Digest of the font installer
-    hashsum = "d3bd48162d91322c3d2861cdccc538955336eff7f0fe50eeafee1b7551a52152"
+    hashsum = 'd3bd48162d91322c3d2861cdccc538955336eff7f0fe50eeafee1b7551a52152'
 
-    if os.path.isfile(f"{util.protonprefix()}/drive_c/windows/Fonts/Overlock-Mod.ttf"):
-        log.info("Overlock-Mod.ttf already installed in prefix")
+    if os.path.isfile(f'{util.protonprefix()}/drive_c/windows/Fonts/Overlock-Mod.ttf'):
+        log.info('Overlock-Mod.ttf already installed in prefix')
         return
 
-    if not os.path.isfile(f"{install_dir}/font/{font_installer}"):
+    if not os.path.isfile(f'{install_dir}/font/{font_installer}'):
         log.warn(f"Could not find '{font_installer}' in '{install_dir}', skipping...")
         return
 
-    with open(f"{install_dir}/font/{font_installer}", mode="rb") as file:
+    with open(f'{install_dir}/font/{font_installer}', mode='rb') as file:
         if sha256(file.read()).hexdigest() != hashsum:
-            log.warn(f"Digest mismatched: {font_installer}")
+            log.warn(f'Digest mismatched: {font_installer}')
             log.warn(f"Expected '{hashsum}', skipping...")
             return
 
     log.info("Installing font 'Overlock-Mod.ttf' in prefix")
     retc = run(
-        [wine, "start", "/unix", f"{install_dir}/font/{font_installer}", "/silent"],
+        [wine, 'start', '/unix', f'{install_dir}/font/{font_installer}', '/silent'],
         check=False,
         env=env,
     ).returncode
