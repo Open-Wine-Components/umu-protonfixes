a3c0d7d7fb54b148b9fc9584aaaee93c2d06a2ff
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-09-12 19:31:28 -0400

    Update Elden Ring fix
    Updated for another issue that has emerged in 1.14

--- gamefixes-steam/1245620.py
+++ gamefixes-steam/1245620.py
@@ -1,10 +1,12 @@
-"""Game fix for Elden Ring: Manually create the `DLC.bdt` file to work around the "Inappropriate activity detected" error for players that don't own the DLC"""
+"""Game fix for Elden Ring: Create the `DLC.bdt` and `DLC.bhd` files to work around the "Inappropriate activity detected" error for players that don't own the DLC"""
 
 from pathlib import Path
 from protonfixes import util
 
-
 def main() -> None:
+    game_dir = Path(util.get_game_install_path()) / 'Game'
     # Create the DLC.bdt file if it doesn't already exist, which is known to fix Easy AntiCheat not working for players that don't own the DLC
     # A blank file is enough to get multiplayer working
-    Path(f'{util.get_game_install_path()}/Game/DLC.bdt').touch(exist_ok=True)
+    (game_dir / 'DLC.bdt').touch(exist_ok=True)
+    # Now also needs DLC.bhd after 1.14 patch
+    (game_dir / 'DLC.bhd').touch(exist_ok=True)
