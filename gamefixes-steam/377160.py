d9cf4ec7b50c2aa5d18204fc0c2b06d39f36dd2e
Fabian Arndt <fabian.arndt@root-core.net>
2024-09-21 02:19:04 +0200

    Mod support for multiple Bethesda games
    - Fallout 3 - Game of the Year Edition
    - Fallout 4
    - Fallout: New Vegas
    - The Elder Scrolls IV: Oblivion
    - The Elder Scrolls V: Skyrim
    - The Elder Scrolls V: Skyrim Special Edition
    - Starfield
    
    This adds support for modorganizer2 and manually installed script runners.
    
    Closes #103

--- gamefixes-steam/377160.py
+++ gamefixes-steam/377160.py
@@ -1,11 +1,1 @@
-"""Game fix for Fallout 4"""
-
-import os
-from protonfixes import util
-
-
-def main() -> None:
-    """Run script extender if it exists."""
-    # Fixes the startup process.
-    if os.path.isfile(os.path.join(os.getcwd(), 'f4se_loader.exe')):
-        util.replace_command('Fallout4Launcher.exe', 'f4se_loader.exe')
+22330.py
