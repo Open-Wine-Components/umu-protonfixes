573da0d36b68881ee41771fc47616de9aa0d1b65
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-09-29 10:06:16 -0700

    fix: remove d3dx11_42 verb for Metro 2033
    - Since https://github.com/ValveSoftware/wine/commit/5d34c6c0dd1618b4da8eec017da3f8231d753bd9 the built-in DLL(s) should work fine in Proton's wine

--- gamefixes-steam/43110.py
+++ gamefixes-steam/43110.py
@@ -4,7 +4,5 @@ from protonfixes import util
 
 
 def main() -> None:
-    """Installs d3dx11_42"""
-    # Fixes D3D10 and D3D11 render path crash on launch.
-    util.protontricks('d3dx11_42')
+    """Installs d3dcompiler_42"""
     util.protontricks('d3dcompiler_42')
