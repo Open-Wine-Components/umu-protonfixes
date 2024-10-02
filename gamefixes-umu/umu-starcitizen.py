30df3264582964ffa2f636f5b40ac89fc8dc5fda
mactan-sc <77816662+mactan-sc@users.noreply.github.com>
2024-10-02 17:13:24 -0500

    star citizen gamefixes

--- gamefixes-umu/umu-starcitizen.py
+++ gamefixes-umu/umu-starcitizen.py
@@ -4,13 +4,12 @@ from protonfixes import util
 
 
 def main() -> None:
-    """EAC Workaround"""
-    # eac workaround
-    util.set_environment('EOS_USE_ANTICHEATCLIENTNULL', '1')
-
     # patch libcuda to workaround crashes related to DLSS
     # See: https://github.com/jp7677/dxvk-nvapi/issues/174#issuecomment-2227462795
     util.patch_libcuda()
 
     # RSI Launcher depends on powershell
     util.protontricks('powershell')
+
+    # RSI Launcher animation
+    util.winedll_override("libglesv2", "builtin")
