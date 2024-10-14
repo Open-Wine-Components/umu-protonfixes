b9de05afc1f5ca6039d6c59a3f979888e7255a27
Fabian Arndt <fabian.arndt@root-core.net>
2024-10-14 23:06:02 +0200

    Added docstrings, linter and type fixes
    - Changed ruff's docstring style to "google"
    - Removed ignored ruff rules, that are already satisfied
    - Ignore missing cls annotations (ANN102)
    - Added docstrings to Config and ConfigBase (+Engine)

--- util.py
+++ util.py
@@ -14,8 +14,8 @@ import functools
 from io import TextIOWrapper
 from pathlib import Path
 from socket import socket, AF_INET, SOCK_DGRAM
-from typing import Literal, Any, Callable, Union, Optional
-from collections.abc import Mapping, Generator
+from typing import Literal, Any, Union, Optional
+from collections.abc import Mapping, Generator, Callable
 
 try:
     from .logger import log
