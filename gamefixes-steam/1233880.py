80e4dfc59aa05962ac0478344408b86b36315a26
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-10-17 13:03:43 -0400

    Add Disgaea 4 fix
    There is a regression in Proton 8 that causes the game to crash at launch unless Esync and Fsync are disabled, and it doesn't set these by default.

---
+++ gamefixes-steam/1233880.py
@@ -0,0 +1,8 @@
+"Game fix for Disgaea 4 Complete+"
+
+from protonfixes import util
+
+def main():
+	"Usually won't reach menu unless Esync and Fsync are disabled."
+	util.disable_esync()
+	util.disable_fsync()
