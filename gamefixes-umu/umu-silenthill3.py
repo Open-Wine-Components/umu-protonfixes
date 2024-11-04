ec14c93254568c2213b9a3f61908b88a19722441
GloriousEggroll <gloriouseggroll@gmail.com>
2024-11-04 12:55:27 -0700

    update silent hill 3 fixes. paired with patched in workaround in proton resolves #8

--- gamefixes-umu/umu-silenthill3.py
+++ gamefixes-umu/umu-silenthill3.py
@@ -7,3 +7,7 @@ def main() -> None:
 
     # Set SteamGameId so that non-steam versions can pick up steam-specific fixes in proton's wine code
     util.set_environment('SteamGameId', 'silenthill3')
+
+    # Needs dsdmo for some cutscenes
+    util.protontricks('directmusic')
+    util.winedll_override("dsound", "builtin")
