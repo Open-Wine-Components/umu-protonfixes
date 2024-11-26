dec04c534bfa7db14bb8f60f1f3543328f43de1f
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-11-25 19:34:14 -0500

    Remove no longer needed Disgaea 4 fix (#162)
    This workaround has now been added to Proton Experimental.

--- gamefixes-steam/1233880.py
+++
@@ -1,8 +0,0 @@
-"""Game fix for Disgaea 4 Complete+"""
-
-from protonfixes import util
-
-def main() -> None:
-	"""Usually won't reach menu unless Esync and Fsync are disabled"""
-	util.disable_esync()
-	util.disable_fsync()
