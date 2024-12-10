2b84ca6c13036da31b660d33f57584134d268ba9
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-12-10 15:26:08 -0500

    Disable Esync and Fsync for Supreme Commander (#167)
    * Add Supreme Commander Fix
    
    Needs Esync and Fsync disabled
    
    * Add Supreme Commander: Forged Alliance fix
    
    * Fix symlink

---
+++ gamefixes-steam/9350.py
@@ -0,0 +1,9 @@
+"""Game fix for Supreme Commander"""
+
+from protonfixes import util
+
+
+def main() -> None:
+	"""Bad performance unless Esync and Fsync are disabled."""
+	util.disable_esync()
+	util.disable_fsync()
