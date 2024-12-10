bdf563517d4551fa838c6e47f6398ba3f89db967
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-12-10 19:39:02 +0000

    Fix symlink

--- gamefixes-steam/9420.py
+++ gamefixes-steam/9420.py
@@ -1,9 +1,1 @@
-"""Game fix for Supreme Commander"""
-
-from protonfixes import util
-
-
-def main() -> None:
-	"""Bad performance unless Esync and Fsync are disabled."""
-	util.disable_esync()
-	util.disable_fsync()
+9350.py
