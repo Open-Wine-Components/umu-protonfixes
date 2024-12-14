d4a02e2dfe995ae402f5d8d68fd664e8fabb1832
kasha695 <67257234+kasha695@users.noreply.github.com>
2024-12-14 20:39:34 +0000

    Remove exe redirect
    Remove bypassing the launcher, launcher related issues have been fixed

--- gamefixes-steam/377840.py
+++ gamefixes-steam/377840.py
@@ -4,9 +4,5 @@ from protonfixes import util
 
 
 def main() -> None:
-    """Changes the proton argument from the launcher to the game"""
     # Fix crackling audio
     util.set_environment('PULSE_LATENCY_MSEC', '60')
-
-    # Replace launcher with game exe in proton arguments
-    util.replace_command('FF9_Launcher.exe', 'x64/FF9.exe')
