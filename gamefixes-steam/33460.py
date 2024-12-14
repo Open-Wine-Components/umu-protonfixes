b3a7358abd8210174b57ff1611fc9c16d22a4d2b
kasha695 <67257234+kasha695@users.noreply.github.com>
2024-12-14 20:33:13 +0000

    Create fix for From Dust (33460)
    Fix the loading issue for From Dust.

---
+++ gamefixes-steam/33460.py
@@ -0,0 +1,9 @@
+"""Game fix for From Dust"""
+
+from protonfixes import util
+
+
+def main() -> None:
+	"""Game will get stuck on initial loading screen unless these are disabled"""
+	util.disable_esync()
+	util.disable_fsync()
