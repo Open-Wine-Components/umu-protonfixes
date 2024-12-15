b3d1d5f4d64f952ad895500f2387d01d3fe74075
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-12-15 12:19:24 -0500

    Workaround for bad Nvidia performance in Indiana Jones and the Great Circle (#172)
    * Add Indiana Jones fix
    
    * Update 2677660.py

---
+++ gamefixes-steam/2677660.py
@@ -0,0 +1,9 @@
+"""Game fix for Indiana Jones and the Great Circle"""
+
+from protonfixes import util
+
+
+def main() -> None:
+	"""Bad performance on Nvidia due to poor VRAM utilization"""
+	util.set_environment('DXVK_NVAPI_GPU_ARCH', 'GA100')
+	util.set_environment('__GL_13ebad', '0x1')
