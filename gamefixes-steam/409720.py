75f2319affcbdaf60d14e705b181f7dbc0919a72
kasha695 <67257234+kasha695@users.noreply.github.com>
2024-12-15 20:09:04 +0000

    Correct the space error

--- gamefixes-steam/409720.py
+++ gamefixes-steam/409720.py
@@ -5,7 +5,6 @@ from protonfixes import util
 
 def main() -> None:
     """Disable ESYNC and FSYNC"""
-
     # ESYNC and FSYNC causes low quality texture problems in all BioShock 1 and 2 versions (Original and Remastered).
     util.disable_esync()
     util.disable_fsync()
