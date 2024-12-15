9d68c341e88c3c5a72817f42c48673d36063968c
kasha695 <67257234+kasha695@users.noreply.github.com>
2024-12-15 19:49:24 +0000

    Update 409720.py

--- gamefixes-steam/409720.py
+++ gamefixes-steam/409720.py
@@ -6,6 +6,6 @@ from protonfixes import util
 def main() -> None:
     """Disable ESYNC and FSYNC"""
 
-    # ESYNC and FSYNC causes low quality texture problems and frequent hangs.
+    # ESYNC and FSYNC causes low quality texture problems in all BioShock 1 and 2 versions (Original and Remastered).
     util.disable_esync()
     util.disable_fsync()
