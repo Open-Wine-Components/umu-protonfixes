1713b9fdbdc8173c8414558cc5231f7ea916fa4a
Arias800 <24809312+Arias800@users.noreply.github.com>
2024-12-19 17:51:31 +0100

    Update umu-517710.py

--- gamefixes-egs/umu-517710.py
+++ gamefixes-egs/umu-517710.py
@@ -1,1 +1,8 @@
-../gamefixes-steam/517710.py
+"""Game fix Redout: Enhanced Edition"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    # Requires vcrun2019 to launch
+    util.protontricks('vcrun2019')
