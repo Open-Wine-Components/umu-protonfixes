5e9b3dee31a19ff7148f67a79b99d9bd74a21265
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-12-20 00:30:31 +0000

    Fixes for AC Syndicate and AC 2

---
+++ gamefixes-steam/368500.py
@@ -0,0 +1,8 @@
+"""Game fix for Assassin's Creed: Syndicate"""
+
+from protonfixes import util
+
+
+def main() -> None:
+	"""Game ships with outdated and not working uPlay launcher."""
+	util.protontricks('ubisoftconnect')
