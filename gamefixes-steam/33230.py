242553de7a7b8097b86d9245ed47c320ccddf565
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2024-12-20 17:14:50 +0000

    Fix AC fixes

--- gamefixes-steam/33230.py
+++ gamefixes-steam/33230.py
@@ -1,1 +1,8 @@
-368500.py
+"""Game fix for Assassin's Creed 2"""
+
+from protonfixes import util
+
+
+def main() -> None:
+	"""Game ships with outdated and not working uPlay launcher."""
+	util.protontricks('ubisoftconnect')
