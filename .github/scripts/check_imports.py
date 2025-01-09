a7ccaac977818d064d4dd67ec9cd74a3640f4f15
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-01-09 13:39:34 -0800

    check_imports: fix PYTHONPATH

--- .github/scripts/check_imports.py
+++ .github/scripts/check_imports.py
@@ -24,7 +24,12 @@ async def main() -> None:  # noqa: D103
     py_bin = which('python')
 
     # Ensure this module is in PYTHONPATH
-    sys.path = [str(project.parent.parent), *sys.path]
+    sys.path = [
+        str(project.parent.parent),
+        str(project.parent),
+        str(project),
+        *sys.path,
+    ]
     print(f'PYTHONPATH: {sys.path}')
 
     if not py_bin:
