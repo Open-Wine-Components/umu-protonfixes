8ee810ea13dfba400a6b231dd0ae6c5c264d1178
Blisto91 <Blisto@protonmail.com>
2025-01-11 01:20:00 +0100

    Remove fix for Marvel Rivals
    The crash when running without SteamDeck=1 have been fixed in Proton Bleeding Edge

--- gamefixes-steam/2767030.py
+++
@@ -1,8 +0,0 @@
-"""Game fix for Marvel Rivals"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    """Game needs SteamDeck=1 to start since season 1"""
-    util.set_environment('SteamDeck', '1')
