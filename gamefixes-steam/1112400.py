2f969d8aeea17c979cc8b3dbab1e7f47da7dfb61
Katharine Chui <kwchuiaa@connect.ust.hk>
2025-01-13 16:03:08 +0100

    Add Project Torque (steam 1112400) workaround (#210)
    https://store.steampowered.com/app/1112400/Project_Torque__Free_2_Play_MMO_Racing_Game
    https://bugs.winehq.org/show_bug.cgi?id=57666

---
+++ gamefixes-steam/1112400.py
@@ -0,0 +1,9 @@
+"""Game fix for Project Torque"""
+
+from protonfixes import util
+
+def main() -> None:
+    """Game needs dotnet35 to launch"""
+    """https://bugs.winehq.org/show_bug.cgi?id=57666"""
+    util.protontricks('dotnet35sp1')
+    util.protontricks('win10')
