9cab12a4c47043669737f853c264ed91bac72c20
Glorious Eggroll <gloriouseggroll@gmail.com>
2025-02-24 12:44:45 -0700

    Add grabfullscreen=y for helldivers 2, fixes #240

---
+++ gamefixes-steam/553850.py
@@ -0,0 +1,9 @@
+"""Helldivers 2"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    """Installs prevent cursor from leaving window in borderless mode
+    """
+    util.protontricks('grabfullscreen=y')
