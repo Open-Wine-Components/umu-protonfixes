88fe5fb1c761f94724457311295ca1c9d9927021
eskay993 <bzboulos@gmail.com>
2025-03-02 10:36:19 +0000

    Add files via upload
    Add fix for extremely loud audio glitches when using Spy Drones and when in hangar section.

---
+++ gamefixes-steam/6920.py
@@ -0,0 +1,9 @@
+"""Deus Ex: Invisible War"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    # Fix for extremely loud audio glitches when using Spy Drones
+    util.protontricks('dsound')
+    
