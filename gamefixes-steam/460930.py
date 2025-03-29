727045e299289f6dc3b4c34947f843a1698c7346
GloriousEggroll <gloriouseggroll@gmail.com>
2025-03-29 12:12:47 -0600

    add ubisoftconnect protonfix for R6Siege and Ghostrecon Wildlands. fixes

--- gamefixes-steam/460930.py
+++ gamefixes-steam/460930.py
@@ -1,10 +1,8 @@
-"""Game fix for Rainbow Six Siege"""
+"""Game fix for Ghost Recon WIldlands"""
 
 from protonfixes import util
 
 
 def main() -> None:
     """Game ships with outdated and not working uPlay launcher."""
-    util.disable_uplay_overlay()
-    util.set_environment('vk_x11_override_min_image_count', '2')
     util.protontricks('ubisoftconnect')
