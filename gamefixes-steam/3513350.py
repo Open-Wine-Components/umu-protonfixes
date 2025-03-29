f8c526793a3054f390ccc9dc0ffc02bdafdf309a
Renaud Lepage <root+GitHub@cybikbase.com>
2025-03-28 19:21:00 -0700

    Fix lint

--- gamefixes-steam/3513350.py
+++ gamefixes-steam/3513350.py
@@ -6,11 +6,11 @@ from protonfixes import util
 
 
 def main() -> None:
-    """Video playback glitches in 2.1+ content
-       This disables Proton mfplat at the cost
-       of in-game experience for now.
-    """
-    util.winedll_override('mfplat', 'd')
-    """In-game browser fix.
-    """
-    util.wineexe_override('KRSDKExternal', 'd')
+   """Video playback glitches in 2.1+ content
+   This disables Proton mfplat at the cost
+   of in-game experience for now.
+   """
+   util.winedll_override('mfplat', 'd')
+   """In-game browser fix.
+   """
+   util.wineexe_override('KRSDKExternal', 'd')
