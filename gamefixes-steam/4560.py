70cfb2286a597bca20ddff5c5125e7843310fd76
GloriousEggroll <gloriouseggroll@gmail.com>
2025-03-30 10:40:31 -0600

    add fix for transformers: war for cybertron (untested, game no longer available on steam or anywhere else)

---
+++ gamefixes-steam/4560.py
@@ -0,0 +1,9 @@
+u"""Game fix for Transformers: War for Cybertron"""
+
+
+from protonfixes import util
+
+
+def main() -> None:
+    """Installs physx library, without it the game simply won't launch"""
+    util.protontricks('physx')
