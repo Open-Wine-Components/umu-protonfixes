184c3c87c036ee2208146991724a46f3c5c89240
Renaud Lepage <root@cybikbase.com>
2025-03-30 10:35:51 -0700

    Moved to umu and added font bits

--- gamefixes-umu/umu-3513350.py
+++ gamefixes-umu/umu-3513350.py
@@ -13,3 +13,12 @@ def main() -> None:
    util.winedll_override('mfplat', 'd')
    """In-game browser fix."""
    util.wineexe_override('KRSDKExternal', 'd')
+   """Font fixes for in-game resources, if any."""
+   util.protontricks('sourcehansans')
+   util.protontricks('fakechinese')
+   util.protontricks('corefonts')
+   """Set SteamGameId so that non-steam versions can
+   pick up steam-specific fixes in proton's wine code
+   if, when and should such fixes ever land.
+   """
+   util.set_environment('SteamGameId', '3513350')
