fa65851057edfcc4d571622e90224622e12326a7
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2025-04-11 20:42:56 -0400

    Add save import for Metaphor ReFantazio

---
+++ gamefixes-steam/2679460.py
@@ -0,0 +1,8 @@
+"""Metaphor ReFantazio"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    """Imports demo saves into the full game."""
+    util.import_saves_folder(3130330, f'AppData/Roaming/SEGA/METAPHOR/Steam/{util.get_steam_account_id()}', True)
