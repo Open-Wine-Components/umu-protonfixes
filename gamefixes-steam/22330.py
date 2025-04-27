e757acd6ea484e9016b64011349aaec3121f7a09
Reilley <reilleygray@gmail.com>
2025-04-27 23:03:15 +1000

    Add support for The Elder Scrolls IV: Oblivion Remastered

--- gamefixes-steam/22330.py
+++ gamefixes-steam/22330.py
@@ -4,6 +4,7 @@
 - Fallout 4
 - Fallout: New Vegas
 - The Elder Scrolls IV: Oblivion
+- The Elder Scrolls IV: Oblivion Remastered
 - The Elder Scrolls V: Skyrim
 - The Elder Scrolls V: Skyrim Special Edition
 - Starfield
@@ -42,6 +43,7 @@ def get_redirect_name(game_id: str) -> Redirect:
         '22370': ('FalloutLauncher.exe', 'fose_loader.exe'),  # Fallout 3
         '377160': ('Fallout4Launcher.exe', 'f4se_loader.exe'),  # Fallout 4
         '22330': ('OblivionLauncher.exe', 'obse_loader.exe'),  # Oblivion
+        '2623190': ('OblivionRemastered.exe', 'OblivionRemastered/Binaries/Win64/obse64_loader.exe'),  # Oblivion Remastered
         '72850': ('SkyrimLauncher.exe', 'skse_loader.exe'),  # Skyrim
         '489830': ('SkyrimSELauncher.exe', 'skse64_loader.exe'),  # Skyrim SE
         '1716740': ('Starfield.exe', 'sfse_loader.exe'),  # Starfield
