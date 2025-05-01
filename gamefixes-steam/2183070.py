62a0ab11f6e18bce4b7620ef1ebe0b160b174ff5
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-05-01 16:36:13 -0700

    chore: update fix for Tokyo Necro

--- gamefixes-steam/2183070.py
+++ gamefixes-steam/2183070.py
@@ -4,11 +4,5 @@ from protonfixes import util
 
 
 def main() -> None:
-    """Installs xact, disable ESYNC, disable FSYNC"""
-    # Fixes crash after typing then entering or clicking `search` within the game's terminal menu
-    util.protontricks('xact')
-    # Fixes hanging after typing then entering or clicking `search` within the game's terminal menu
-    util.disable_esync()
-    util.disable_fsync()
     # Fixes audio not playing for in-game videos
     util.disable_protonmediaconverter()
