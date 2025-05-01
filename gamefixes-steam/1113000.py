96480a8eee718e594d9b9a72c7c495cbbd460cc0
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-05-01 14:26:27 -0700

    chore: remove fix for Persona 4 Golden
    - Fixed in https://github.com/ValveSoftware/wine/commit/0ead1ac80f89678fc0a9751ff9f4f51a2d18e4e9 and tested using Proton 10.0-1c with the Steam client's shader pre-cache disabled

--- gamefixes-steam/1113000.py
+++
@@ -1,9 +0,0 @@
-"""Game fix for Persona 4 Golden"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    util.protontricks('d3dx11_43')
-    util.protontricks('wmp9')
-    util.protontricks('klite')
