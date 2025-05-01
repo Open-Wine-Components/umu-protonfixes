7cbeead53a05fed2b13198c030f80859f2536f36
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-05-01 23:55:34 +0000

    chore: update fixes for MAGES. engine based titles (#305)
    * chore: remove fix for Full Metal Deamon Muramasa
    
    - Fixed in faudio https://github.com/FNA-XNA/FAudio/releases/tag/24.06 and wine since https://gitlab.winehq.org/wine/wine/-/commit/e8f936c745b24f794b36a0af794086e0f57c8551. See https://bugs.winehq.org/show_bug.cgi?id=54246
    
    * chore: update fix for Mojika - Truth Rears Its Ugly Head
    
    * chore: update fix for YOU and ME and HER: A Love Story
    
    * chore: update fix for Tokyo Necro
    
    * chore: remove fix for The Quintessential Quintuplets - Five Memories Spent With You
    
    * chore: remove fix for The Song of Saya

--- gamefixes-steam/1293820.py
+++ gamefixes-steam/1293820.py
@@ -4,10 +4,5 @@ from protonfixes import util
 
 
 def main() -> None:
-    """Install xact, disable esync, disable fsync"""
-    # Fixes the game from crashing or hanging during intro
-    util.protontricks('xact')
-    util.disable_esync()
-    util.disable_fsync()
     # Fixes audio not playing for in-game videos
     util.disable_protonmediaconverter()
