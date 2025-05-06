f67374f8deedfe7e86ca19292832ea2a68e27cdb
termdisc <outphase@gmail.com>
2025-05-06 12:01:04 -0500

    Add Marvel Rivals (EGS) fixes (#311)
    * Add Marvel Rivals (EGS) fixes
    
    Replace target exe and set `SteamDeck=1`
    
    * Update umu-2767030.py

---
+++ gamefixes-egs/umu-2767030.py
@@ -0,0 +1,13 @@
+"""Game fix for Marvel Rivals"""
+# EGS-ID 575efd0b5dd54429b035ffc8fe2d36d0
+
+from protonfixes import util
+
+
+def main() -> None:
+
+    util.replace_command('epic_launch_helper.exe', 'MarvelRivals_Launcher.exe')
+    
+    # Set SteamDeck=1 to be able to launch the game
+    util.set_environment('SteamDeck', '1')
+
