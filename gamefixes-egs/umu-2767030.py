add844efd2b0ac2baea16ee894e88178f157b8e3
termdisc <outphase@gmail.com>
2025-05-06 11:57:15 -0500

    Update umu-2767030.py

--- gamefixes-egs/umu-2767030.py
+++ gamefixes-egs/umu-2767030.py
@@ -7,9 +7,6 @@ from protonfixes import util
 def main() -> None:
 
     util.replace_command('epic_launch_helper.exe', 'MarvelRivals_Launcher.exe')
-
-    # Set SteamGameId so that non-steam versions can pick up steam-specific fixes in proton's wine code
-    util.set_environment('SteamGameId', '2767030')
     
     # Set SteamDeck=1 to be able to launch the game
     util.set_environment('SteamDeck', '1')
