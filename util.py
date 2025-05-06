52acb0057553b828b6d68d92724470cfeb069fee
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-05-05 20:52:39 -0700

    util: find xrandr in PATH

--- util.py
+++ util.py
@@ -822,10 +822,15 @@ def set_xml_options(
     return True
 
 
-def get_resolution() -> tuple[int, int]:
+def get_resolution() -> Union[tuple[int, int], None]:
     """Returns screen res width, height using xrandr"""
     # Execute xrandr command and capture its output
-    xrandr_bin = os.path.abspath(__file__).replace('util.py', 'xrandr')
+    xrandr_bin = shutil.which('xrandr')
+
+    if not xrandr_bin:
+        log.info('xrandr not found in PATH, skipping screen resolution determination')
+        return None
+
     xrandr_output = subprocess.check_output([xrandr_bin, '--current']).decode('utf-8')
 
     # Find the line that starts with 'Screen   0:' and extract the resolution
