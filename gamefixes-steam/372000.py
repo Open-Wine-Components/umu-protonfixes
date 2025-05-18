df08a1c5e1bddde5201db92aff4863fdd95ce50e
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2025-05-18 15:38:54 -0400

    Install `quartz` for *Tree of Savior* (#325)

--- gamefixes-steam/372000.py
+++ gamefixes-steam/372000.py
@@ -6,3 +6,5 @@ from protonfixes import util
 def main() -> None:
     """https://forum.treeofsavior.com/t/linux-the-graphic-card-does-not-support-directx11-13ep/418073/13"""
     util.protontricks('d3dcompiler_47')
+    # Installing Quartz allows the game to work on GE 10-1
+    util.protontricks('quartz')
