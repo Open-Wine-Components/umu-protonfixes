34b9208e3e8426fed2b1a62b362a941a4be47b0f
Jack Greiner <jack@emoss.org>
2025-06-08 01:12:16 -0400

    patch_libcuda: Slightly refactor how environment variables are set.
    This is cleaner and makes reading this easier.

--- util.py
+++ util.py
@@ -588,16 +588,12 @@ def patch_libcuda() -> bool:
         log.info(f'Patched libcuda.so saved to: {patched_library}')
 
         # Set LD_LIBRARY_PATH and LD_PRELOAD to include the directory (or absolute path) of the patched library
-        current_ld_path = protonmain.g_session.env.get("LD_LIBRARY_PATH")
-        protonmain.g_session.env["LD_LIBRARY_PATH"] = (
-            f"{patched_library.parent}:{current_ld_path}" if current_ld_path else str(patched_library)
-        )
+        ld_paths = [str(patched_library.parent), protonmain.g_session.env.get("LD_LIBRARY_PATH")]  
+        protonmain.g_session.env["LD_LIBRARY_PATH"] = ":".join(filter(None, ld_paths))  
         log.info(f'LD_LIBRARY_PATH updated to {protonmain.g_session.env.get("LD_LIBRARY_PATH")}')
 
-        current_ld_preload = protonmain.g_session.env.get("LD_PRELOAD")
-        protonmain.g_session.env["LD_PRELOAD"] = (
-            f"{patched_library}:{current_ld_preload}" if current_ld_preload else str(patched_library)
-        )
+        ld_paths = [str(patched_library), protonmain.g_session.env.get("LD_PRELOAD")]  
+        protonmain.g_session.env["LD_PRELOAD"] = ":".join(filter(None, ld_paths))
         log.info(f'LD_PRELOAD updated to {protonmain.g_session.env.get("LD_PRELOAD")}')
         return True
 
