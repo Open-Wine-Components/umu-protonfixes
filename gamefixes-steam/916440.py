60ec49fec3078d293e886ab1b0ac6bf71fbdc135
qubist-pixel-ux <74808402+qubist-pixel-ux@users.noreply.github.com>
2025-06-18 13:00:53 +0530

    Anno 1800 : Do not disable overlay

--- gamefixes-steam/916440.py
+++ gamefixes-steam/916440.py
@@ -6,5 +6,3 @@ from protonfixes import util
 def main() -> None:
     """Game ships with outdated uPlay launcher, which will not install to be able to start the game"""
     util.protontricks('ubisoftconnect')
-    """Disable Overlay as it is known to cause issues"""
-    util.disable_uplay_overlay()
