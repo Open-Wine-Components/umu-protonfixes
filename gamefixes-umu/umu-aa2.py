02ce2f7d2b5fe5191970b940101a502af2c8b302
GloriousEggroll <gloriouseggroll@gmail.com>
2025-06-24 16:31:53 -0600

    we do not need to be calling dxvk for protonfixes, thats handled via proton

--- gamefixes-umu/umu-aa2.py
+++ gamefixes-umu/umu-aa2.py
@@ -5,8 +5,7 @@ from protonfixes import util
 
 
 def main() -> None:
-    """Installs fakejapanese_ipamona vcrun2015 dotnetcore3 dxvk"""
+    """Installs fakejapanese_ipamona vcrun2015 dotnetcore3"""
     util.protontricks('vcrun2015')
     util.protontricks('dotnetcore3')
-    util.protontricks('dxvk')
     util.protontricks('fakejapanese_ipamona')
