dd3c36901318250756562e2d23be05d4e4918a96
UsernamesAreNotMyThing <92757918+UsernamesAreNotMyThing@users.noreply.github.com>
2025-07-06 18:46:02 -0400

    Fix bad import statement

--- gamefixes-steam/2138710.py
+++ gamefixes-steam/2138710.py
@@ -1,8 +1,8 @@
 """Sifu"""
 
-from protonfixes import util, BasePath
+from protonfixes import util
 
 
 def main() -> None:
     """Stops the game from freezing."""
-    util.set_ini_options('[/script/engine.renderersettings]\nr.CreateShadersOnLoad=1', 'Sifu/Saved/Config/WindowsClient/Engine.ini', base_path = BasePath.APPDATA_LOCAL)
+    util.set_ini_options('[/script/engine.renderersettings]\nr.CreateShadersOnLoad=1', 'Sifu/Saved/Config/WindowsClient/Engine.ini', base_path = util.BasePath.APPDATA_LOCAL)
