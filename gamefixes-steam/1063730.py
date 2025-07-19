e7595a30db5c98206ce17650fb400eecf5d72b43
GloriousEggroll <gloriouseggroll@gmail.com>
2025-07-18 22:32:21 -0600

    remove deprecated core count fix for new world

--- gamefixes-steam/1063730.py
+++
@@ -1,9 +0,0 @@
-"""Game fix for New World"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    """Needs core count limit"""
-    # Fix the startup process:
-    util.set_cpu_topology_limit(12)
