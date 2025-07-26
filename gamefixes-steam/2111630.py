369089648231a6ac336cae02c8aa2c5e778ba42a
GloriousEggroll <gloriouseggroll@gmail.com>
2025-07-25 21:17:30 -0600

    fix broken workflow

--- gamefixes-steam/2111630.py
+++ gamefixes-steam/2111630.py
@@ -6,6 +6,4 @@ from protonfixes import util
 def main() -> None:
     """Add misc. fixes"""
     util.protontricks('d3dcompiler_43')
-    util.protontricks('msvcm90')
-    util.protontricks('msvcp90')
-    util.protontricks('msvcr90')
+    util.protontricks('vcrun2008')
