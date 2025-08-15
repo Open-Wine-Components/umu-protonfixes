5e4c01317cfac4a4c25fad3f0b2743acc6b996fe
GloriousEggroll <gloriouseggroll@gmail.com>
2025-08-15 17:52:28 -0600

    for DMC HD Collection to use winegstreamer

---
+++ gamefixes-steam/631510.py
@@ -0,0 +1,9 @@
+"""Game fix for Devil May Cry HD Collection
+"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    # Force using winegstreamer instead of winedmo for video playback
+    util.set_environment('PROTON_MEDIA_USE_GST', '1')
