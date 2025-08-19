e408565bbc321c851a77d554cad4f8e7b3bedab0
RoGreat <roguegreat@gmail.com>
2025-08-19 01:12:34 -0500

    Restore sound for ONE PIECE: PIRATE WARRIORS 4

---
+++ gamefixes-steam/1089090.py
@@ -0,0 +1,10 @@
+"""ONE PIECE: PIRATE WARRIORS 4
+Missing voices/sounds in cutscenes
+Requires disabling the gstreamer protonaudioconverterbin plugin to get full audio in cutscenes.
+"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    util.disable_protonmediaconverter()
