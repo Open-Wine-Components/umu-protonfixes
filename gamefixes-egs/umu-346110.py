72a5acc5eba4efd978d6203ccce6d12cec5b577f
Mightymike033 <knoes64@gmail.com>
2025-08-26 14:59:52 +0200

    video playback workaround for ARK: Survival Evolved

---
+++ gamefixes-egs/umu-346110.py
@@ -0,0 +1,1 @@
+../gamefixes-steam/346110.py
---
+++ gamefixes-steam/346110.py
@@ -0,0 +1,7 @@
+"""ARK: Survival Evolved"""
+
+from protonfixes import util
+
+def main() -> None:
+    """Video Playback Fix"""
+    util.set_environment('PROTON_MEDIA_USE_GST', '1')
