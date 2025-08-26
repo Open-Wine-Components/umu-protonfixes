9eff52ebf0fdbb3b0ecca114ab1042aaaba65554
GloriousEggroll <gloriouseggroll@gmail.com>
2025-08-26 08:57:51 -0600

    add ue4ss and mod enabler detection for wuchang

---
+++ gamefixes-steam/2277560.py
@@ -0,0 +1,22 @@
+"""Mod support for Wuchang:
+
+"""
+
+import os
+import glob
+from protonfixes import util
+
+
+def main() -> None:
+    """Enable modding and fixes"""
+
+    install_dir = glob.escape(util.get_game_install_path())
+
+    # UE4SS
+    if os.path.exists(install_dir + '/Project_Plague/Binaries/Win64/ue4ss'):
+        util.winedll_override('dwmapi', util.OverrideOrder.NATIVE_BUILTIN)
+
+    # Wuchang Mod Enabler
+    if os.path.exists(install_dir + '/Project_Plague/Binaries/Win64/bitfix'):
+        util.winedll_override('dsound', util.OverrideOrder.NATIVE_BUILTIN)
+
