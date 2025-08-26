b82695ea3f7b64bb7304d2a4e900f8ea76146103
GloriousEggroll <gloriouseggroll@gmail.com>
2025-08-26 10:55:35 -0600

    remove deprecated helldivers 2 cursor grab workaround, resolves #373

--- gamefixes-steam/553850.py
+++
@@ -1,8 +0,0 @@
-"""Helldivers 2"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    """Installs prevent cursor from leaving window in borderless mode"""
-    util.protontricks('grabfullscreen=y')
