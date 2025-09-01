75dffb3bf2d3600b880de77eea80050a8cad8b1a
Bitwolf <65789901+Bitwolfies@users.noreply.github.com>
2025-09-01 12:52:58 -0700

    STORY OF SEASONS: Pioneers of Olive Town boot fix. (#390)
    Gets the game booting, though it doesn't fix the non working DLC, some sorta casefolding issue probably?

---
+++ gamefixes-steam/1392960.py
@@ -0,0 +1,9 @@
+"""Game fix for STORY OF SEASONS: Pioneers of Olive Town"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    #Wont boot without them, sadly doesn't fix DLC not actually loading in game, casefolding issue?
+    util.protontricks('d3dcompiler_43')
+    util.protontricks('d3dcompiler_47')
