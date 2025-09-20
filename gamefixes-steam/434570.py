d4b8a8848c9f34acc32623bbacc1d1f3e750ea53
GloriousEggroll <gloriouseggroll@gmail.com>
2025-09-20 04:04:17 -0600

    remove deprecated blood & bacon fix, closes #395

--- gamefixes-steam/434570.py
+++
@@ -1,9 +0,0 @@
-"""Game fix for Blood & Bacon"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    """Add misc. fixes"""
-    util.protontricks('dotnet40')
-    util.protontricks('xna40')
