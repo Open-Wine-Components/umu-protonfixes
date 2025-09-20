3f7ab487782bbf96a7705f0e4f81726beebf3a9f
GloriousEggroll <gloriouseggroll@gmail.com>
2025-09-20 02:10:22 -0600

    add ue4ss support for stellar blade

---
+++ gamefixes-steam/3489700.py
@@ -0,0 +1,17 @@
+"""Mod support for Stellar Blade
+
+"""
+
+import os
+import glob
+from protonfixes import util
+
+
+def main() -> None:
+    """Enable modding and fixes"""
+
+    install_dir = glob.escape(util.get_game_install_path())
+
+    # UE4SS
+    if os.path.exists(install_dir + '/SB/Binaries/Win64/ue4ss'):
+        util.winedll_override('dwmapi', util.OverrideOrder.NATIVE_BUILTIN)
