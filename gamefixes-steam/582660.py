97e668d9b50e30eaa9a2eb7a0df11d2fc8c885e4
GloriousEggroll <gloriouseggroll@gmail.com>
2025-10-14 17:21:04 -0600

    add fix for broken launcher rendering in Black Desert

--- gamefixes-steam/582660.py
+++ gamefixes-steam/582660.py
@@ -11,3 +11,13 @@ def main() -> None:
         util.replace_command('--steam', '')
     # Needed for Launcher
     util.set_environment('WINE_DISABLE_KERNEL_WRITEWATCH', '1')
+    configpath = os.path.join(
+        util.protonprefix(),
+        'drive_c/users/steamuser/My Documents/Black Desert',
+    )
+    if not os.path.exists(configpath):
+        os.makedirs(configpath)
+    configgame = os.path.join(configpath, 'GameOptionLauncher.txt')
+    if not os.path.isfile(configgame):
+        with open(configgame, 'w+', encoding='utf-8') as f:
+            f.write('launcherGpu = 0\nlauncherOffScreen = 0')
