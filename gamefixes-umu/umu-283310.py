195504f54bda9a689cade93500215475f291cc7b
matthymd <matthias_dillen@telenet.be>
2025-10-28 00:50:45 +0100

    formatting

--- gamefixes-umu/umu-283310.py
+++ gamefixes-umu/umu-283310.py
@@ -5,5 +5,6 @@ from protonfixes import util
 
 def main() -> None:
     """Requires no-cd patch, disc does not get detected properly and keeps Wine hanging with sync:RtlpWaitForCriticalSection.
-    Disable eax in advanced settings or the game won't run either."""
+    Disable eax in advanced settings or the game won't run either.
+    """
     util.protontricks('mfc42')
