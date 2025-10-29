625dd9700c64210ef81162e6fd2a846463ff2573
GloriousEggroll <gloriouseggroll@gmail.com>
2025-10-28 21:53:34 -0600

    fix broken space engineers launch

--- gamefixes-steam/244850.py
+++ gamefixes-steam/244850.py
@@ -6,12 +6,13 @@ from protonfixes import util
 def main() -> None:
     util.protontricks('xaudio29')
     util.protontricks('dotnet48')
+    util.protontricks('vcrun2015')
 
-    base_attibutte = '<runtime>'
+    base_attribute = '<runtime>'
     game_opts = """
   	<gcServer enabled = "true" />
 """
 
-    util.set_xml_options(base_attibutte, game_opts, 'Bin64/SpaceEngineers.exe.config')
+    util.set_xml_options(base_attribute, game_opts, 'Bin64/SpaceEngineers.exe.config')
 
     util.append_argument('-skipintro')
