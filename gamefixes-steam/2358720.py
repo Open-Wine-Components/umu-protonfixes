565bd88ef40ca23277dbbe9e93d74f198c05d68f
Stelios Tsampas <loathingkernel@gmail.com>
2025-11-25 21:07:41 +0200

    Try to delete the registry key if the GPU is not NVidia

--- gamefixes-steam/2358720.py
+++ gamefixes-steam/2358720.py
@@ -3,16 +3,20 @@
 from protonfixes import util
 
 def main() -> None:
+    # Enable Hardware Scheduling for Frame Generation on Nvidia
+    # https://github.com/jp7677/dxvk-nvapi/wiki/Tips-and-tricks-for-usage-with-DXVK-NVAPI#dlss-frame-generation
     # Causes the game to disable FSR on non-Nvidia GPUS, skip if not on Nvidia. Taken from Proton
     with open('/proc/modules') as f:
         drivers = set([line.partition(' ')[0] for line in f.read().splitlines()])
         if not drivers.intersection({'nvidia', 'nouveau', 'nova'}):
-            return
-    # Enable Hardware Scheduling for Frame Generation on Nvidia
-    # https://github.com/jp7677/dxvk-nvapi/wiki/Tips-and-tricks-for-usage-with-DXVK-NVAPI#dlss-frame-generation
-    util.regedit_add(
-        'HKLM\\System\\CurrentControlSet\\Control\\GraphicsDrivers',
-        'HwSchMode',
-        'REG_DWORD',
-        '2',
-    )
+            util.regedit_delete(
+                'HKLM\\System\\CurrentControlSet\\Control\\GraphicsDrivers',
+                'HwSchMode'
+            )
+        else:
+            util.regedit_add(
+                'HKLM\\System\\CurrentControlSet\\Control\\GraphicsDrivers',
+                'HwSchMode',
+                'REG_DWORD',
+                '2',
+            )
