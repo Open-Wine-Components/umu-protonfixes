e746907c097d1db135286e736619b188ecaa1625
Stelios Tsampas <loathingkernel@gmail.com>
2025-11-25 20:03:46 +0200

    Skip Black Myth Wukong when not using Nvidia

--- gamefixes-steam/2358720.py
+++ gamefixes-steam/2358720.py
@@ -3,6 +3,11 @@
 from protonfixes import util
 
 def main() -> None:
+    # Causes the game to disable FSR on non-Nvidia GPUS, skip if not on Nvidia. Taken from Proton
+    with open('/proc/modules') as f:
+        drivers = set([line.partition(' ')[0] for line in f.read().splitlines()])
+        if not drivers.intersection({'nvidia', 'nouveau', 'nova'}):
+            return
     # Enable Hardware Scheduling for Frame Generation on Nvidia
     # https://github.com/jp7677/dxvk-nvapi/wiki/Tips-and-tricks-for-usage-with-DXVK-NVAPI#dlss-frame-generation
     util.regedit_add(
