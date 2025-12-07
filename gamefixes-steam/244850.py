d91198a865868f8c8b5342ebae2e43e8150ff6b4
GloriousEggroll <gloriouseggroll@gmail.com>
2025-12-06 19:33:07 -0500

    fix space engineers (again). resolves https://github.com/GloriousEggroll/proton-ge-custom/issues/355

--- gamefixes-steam/244850.py
+++ gamefixes-steam/244850.py
@@ -6,13 +6,12 @@ from protonfixes import util
 def main() -> None:
     util.protontricks('xaudio29')
     util.protontricks('dotnet48')
-    util.protontricks('vcrun2015')
+    util.protontricks('vcrun2019')
 
     base_attribute = '<runtime>'
     game_opts = """
-  	<gcServer enabled = "true" />
+    <loadFromRemoteSources enabled="true" />
+    <gcServer enabled = "true" />
 """
 
     util.set_xml_options(base_attribute, game_opts, 'Bin64/SpaceEngineers.exe.config')
-
-    util.append_argument('-skipintro')
