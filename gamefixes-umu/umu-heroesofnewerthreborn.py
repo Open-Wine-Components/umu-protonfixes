189bce3f7bbf75594e029bfc10fae721837dd5d0
GloriousEggroll <gloriouseggroll@gmail.com>
2025-12-09 22:57:20 -0500

    add fix for heroes of newerth reborn

---
+++ gamefixes-umu/umu-heroesofnewerthreborn.py
@@ -0,0 +1,9 @@
+"""Game fix for Heroes of Newerth Reborn"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    util.protontricks('gdiplus')
+    util.protontricks('corefonts')
+    util.protontricks('ie8')
