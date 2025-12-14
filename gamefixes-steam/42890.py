265ad3ca17d24a452e2ee081ea9b71a963bbf14a
GloriousEggroll <gloriouseggroll@gmail.com>
2025-12-14 17:45:50 -0500

    add fix for sword of the stars (fixes #423)

---
+++ gamefixes-steam/42890.py
@@ -0,0 +1,10 @@
+"""Game fix for Sword of the Stars"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    """Installs d3dcompiler_42"""
+    util.protontricks('d3dcompiler_42')
+    util.protontricks('d3dx9_31')
+    util.protontricks('d3dx9_42')
