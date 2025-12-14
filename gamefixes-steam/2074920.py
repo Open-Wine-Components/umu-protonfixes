e432f2f8e97351c5f0812d3488d8eb79bd5655df
GloriousEggroll <gloriouseggroll@gmail.com>
2025-12-14 03:50:50 -0500

    add broken eac workaround for the first descendant

---
+++ gamefixes-steam/2074920.py
@@ -0,0 +1,9 @@
+"""Game fix for The First Descendant"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    """Game Currently has some funkyness where it doesnt properly detect eac, works fine after game is started without connecting. Star Citizen used to need this too
+    """
+    util.set_environment('WINE_BLOCK_HOSTS', 'modules-cdn.eac-prod.on.epicgames.com')
