d4ae944223cb9cc63b90083be5eef76afe72a03d
GloriousEggroll <gloriouseggroll@gmail.com>
2025-12-15 00:26:48 -0500

    make ruff happy

--- gamefixes-steam/2074920.py
+++ gamefixes-steam/2074920.py
@@ -4,6 +4,5 @@ from protonfixes import util
 
 
 def main() -> None:
-    """Game Currently has some funkyness where it doesnt properly detect eac, works fine after game is started without connecting. Star Citizen used to need this too
-    """
+    """Game Currently has some funkyness where it doesnt properly detect eac, works fine after game is started without connecting. Star Citizen used to need this too """
     util.set_environment('WINE_BLOCK_HOSTS', 'modules-cdn.eac-prod.on.epicgames.com')
