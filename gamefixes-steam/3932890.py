7007f0b4fdac3af399f8113ed86bee4f32f2cfa0
Jan200101 <sentrycraft123@gmail.com>
2025-12-18 21:55:51 +0100

    removed unnessary dependencies from tarkov fixes

--- gamefixes-steam/3932890.py
+++ gamefixes-steam/3932890.py
@@ -10,9 +10,6 @@ from protonfixes import util
 def main() -> None:
     util.install_battleye_runtime()
     util.protontricks('dotnet48')
-    util.protontricks('vcrun2022')
-    util.protontricks('dotnetdesktop6')
-    util.protontricks('dotnetdesktop8')
 
     game_dir = glob.escape(util.get_game_install_path())
 
