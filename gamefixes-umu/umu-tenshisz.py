9d654979ba9b39a223ce57f93d715771e166c2d9
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-12-19 18:20:08 -0800

    Update fix for Angelic Chaos: RE-BOOT! (#475)
    A change in Valve's wine media playback code in Proton 10 caused in-game media playback to no longer work and crash the game instead. To restore media playback and not crash, refer to the built-in hack for ATRI -My Dear Moments-, use the gstreamer-based backend, and configure the game's movie rendering.

--- gamefixes-umu/umu-tenshisz.py
+++ gamefixes-umu/umu-tenshisz.py
@@ -5,3 +5,7 @@ from protonfixes import util
 
 def main() -> None:
     util.disable_protonmediaconverter()
+    util.set_environment('PROTON_MEDIA_USE_GST', '1')
+    util.append_argument('-vomstyle=layer')
+    # Refer to the ATRI -My Dear Moments- hack to fix media playback
+    util.set_environment('SteamGameId', '1230140')
