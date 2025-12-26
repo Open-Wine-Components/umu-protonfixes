b84fb672ff921f8d6ad22e46c32a0b5dbf8e9a8e
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2025-12-25 23:27:52 -0300

    Remove deactivation of UPlay overlay

--- gamefixes-steam/359550.py
+++ gamefixes-steam/359550.py
@@ -5,6 +5,5 @@ from protonfixes import util
 
 def main() -> None:
     """Game ships with outdated UPlay launcher, which impedes normal initialization"""
-    util.disable_uplay_overlay()
     util.set_environment('vk_x11_override_min_image_count', '2')
     util.protontricks('ubisoftconnect')
