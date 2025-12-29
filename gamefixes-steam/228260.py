4a9087a06d138fbc7a358c471be48dda3148133c
GloriousEggroll <gloriouseggroll@gmail.com>
2025-12-29 02:10:55 -0500

    add fixes for sorcerer king and fallen enchantress: legendary heroes

---
+++ gamefixes-steam/228260.py
@@ -0,0 +1,12 @@
+"""Game fix for Fallen Enchantress: Legendary Heroes"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    util.protontricks('usp10')
+    util.protontricks('vcrun2015')
+    util.protontricks('d3dx9')
+    util.protontricks('d3dcompiler_43')
+    util.protontricks('d3dcompiler_47')
+    util.winedll_override('SDNXLFonts', util.OverrideOrder.DISABLED)
---
+++ gamefixes-steam/241990.py
@@ -0,0 +1,12 @@
+"""Game fix for Sorcerer King"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    util.protontricks('usp10')
+    util.protontricks('vcrun2015')
+    util.protontricks('d3dx9')
+    util.protontricks('d3dcompiler_43')
+    util.protontricks('d3dcompiler_47')
+    util.winedll_override('SDNXLFonts', util.OverrideOrder.DISABLED)
