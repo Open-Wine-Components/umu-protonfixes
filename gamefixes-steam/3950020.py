bef895358c0a166a0af56bfccce1b13938684a66
GloriousEggroll <gloriouseggroll@gmail.com>
2026-01-01 17:39:18 -0500

    fixup duet night abyss entries, add standalone entry

--- gamefixes-steam/3950020.py
+++ gamefixes-steam/3950020.py
@@ -1,1 +1,8 @@
-../gamefixes-egs/umu-3950020.py
+"""Game fix for Duet Night Abyss"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    """CEF tries to use dcomp by default which only has stubs, this triggers a fallback to a different backend"""
+    util.winedll_override('dcomp', util.OverrideOrder.DISABLED)
