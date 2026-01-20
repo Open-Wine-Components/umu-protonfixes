674a5d047e2ff3dd4b83c1668c71a1f9407bb6bd
Dylan Donnell <dylan.donnell@student.griffith.ie>
2026-01-20 00:27:02 +0000

    add game drive env var for Duet Night Abyss

--- gamefixes-steam/3950020.py
+++ gamefixes-steam/3950020.py
@@ -6,3 +6,4 @@ from protonfixes import util
 def main() -> None:
     """CEF tries to use dcomp by default which only has stubs, this triggers a fallback to a different backend"""
     util.winedll_override('dcomp', util.OverrideOrder.DISABLED)
+    util.set_environment('PROTON_SET_GAME_DRIVE', '1')
