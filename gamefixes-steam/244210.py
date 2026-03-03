cddcb606e2b64fa6842eec17b030a7a3773656f8
Samuel Rounce <srounce@users.noreply.github.com>
2026-03-03 12:24:41 +0000

    fix: Assetto Corsa CM missing dotnet 4.8 prompts
    Currently Content Manager will work but will repeatedly prompt about
    missing dotnet 4.8 when starting CM and the game itself. Some people
    have also reported instability regarding CM functionality without
    dotnet 4.8.

--- gamefixes-steam/244210.py
+++ gamefixes-steam/244210.py
@@ -5,7 +5,7 @@ from protonfixes import util
 
 def main() -> None:
     """Fixes default launcher and ACM."""
-    util.protontricks('dotnet452')
+    util.protontricks('dotnet48')
     # Fixes Content Manager (black windows)
     util.protontricks('d3dx11_43')
     util.protontricks('d3dcompiler_47')
