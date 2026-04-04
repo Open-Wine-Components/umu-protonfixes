45ef7f1587041cc5d5a49f613c92e58153d90985
GloriousEggroll <gloriouseggroll@gmail.com>
2026-04-04 06:01:49 -0600

    remove obsolete akibas trip fix and cleanup invalid darksiders warmastered and darksiders 2 entries

--- gamefixes-gog/umu-333980.py
+++
@@ -1,1 +0,0 @@
-../gamefixes-steam/333980.py
--- gamefixes-steam/333980.py
+++
@@ -1,8 +0,0 @@
-"""Game fix for Akiba's Trip: Undead & Undressed"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    util.protontricks('hidewineexports=enable')
-    util.set_environment('PROTON_MEDIA_USE_GST', '1')
