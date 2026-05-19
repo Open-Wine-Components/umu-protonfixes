0cc5f98a86e759ae737a5b5389e8096ced102599
SinnohConfirmed <SinnohConfirmed@users.noreply.github.com>
2026-05-19 10:06:39 -0400

    add fix for GOG release of Quake II (Original)

---
+++ gamefixes-gog/umu-2320.py
@@ -0,0 +1,10 @@
+"""Game fix for Quake II (Original)"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    # Fix crash at startup when using OpenGL
+    util.set_environment('PROTON_OLD_GL_STRING', '1')
+	# Fix in-game music not playing
+    util.set_environment('WINEDLLOVERRIDES', 'winmm=n,b')
