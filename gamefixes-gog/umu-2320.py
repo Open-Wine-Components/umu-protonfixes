68de2e7547fc49d0545e496587b8d5f7bd45979f
SinnohConfirmed <SinnohConfirmed@users.noreply.github.com>
2026-05-19 15:57:29 -0400

    chnage env variables for OpenGL fix
    PROTON_OLD_GL_STRING=1 doesn't seem to work when in the umu script when testing on a Linux Mint machine. MESA_EXTENSION_MAX_YEAR=2003 and __GL_ExtensionStringVersion=17700 do work.

--- gamefixes-gog/umu-2320.py
+++ gamefixes-gog/umu-2320.py
@@ -5,6 +5,7 @@ from protonfixes import util
 
 def main() -> None:
     # Fix crash at startup when using OpenGL
-    util.set_environment('PROTON_OLD_GL_STRING', '1')
+    util.set_environment('MESA_EXTENSION_MAX_YEAR', '2003')
+    util.set_environment('__GL_ExtensionStringVersion', '17700')
 	# Fix in-game music not playing
     util.winedll_override('winmm', util.OverrideOrder.NATIVE_BUILTIN)
