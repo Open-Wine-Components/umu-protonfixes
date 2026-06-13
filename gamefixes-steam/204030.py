b7024a4317c4a27670affc783ad42d206fe01112
GloriousEggroll <gloriouseggroll@gmail.com>
2026-06-12 23:36:43 -0600

    remove deprecated gstreamer video envvars

--- gamefixes-steam/204030.py
+++
@@ -1,8 +0,0 @@
-"""Fable - The Lost Chapters"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    """Video Playback Fix"""
-    util.set_environment('GST_GL_WINDOW', 'x11')
--- gamefixes-steam/2325290.py
+++
@@ -1,7 +0,0 @@
-"""Game fix for Sky: Children of the Light"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    util.set_environment('PROTON_GST_VIDEO_ORIENTATION', 'vertical-flip')
