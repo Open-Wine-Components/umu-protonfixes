a08c5a6a3cc55055f2ff62c2329a0c5d7c7a59ef
GloriousEggroll <gloriouseggroll@gmail.com>
2026-06-13 11:45:52 -0600

    more deprecated video removals

--- gamefixes-umu/umu-tenshisz.py
+++ gamefixes-umu/umu-tenshisz.py
@@ -4,8 +4,4 @@ from protonfixes import util
 
 
 def main() -> None:
-    util.disable_protonmediaconverter()
-    util.set_environment('PROTON_MEDIA_USE_GST', '1')
     util.append_argument('-vomstyle=layer')
-    # Refer to the ATRI -My Dear Moments- hack to fix media playback
-    util.set_environment('SteamGameId', '1230140')
