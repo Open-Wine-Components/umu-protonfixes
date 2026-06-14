eab66071a9c325449e2f55c5d77953923fd85162
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2026-06-14 03:14:26 -0700

    gamefixes-umu/umu-zwift: refactor implementation
    Fixes https://github.com/Open-Wine-Components/umu-protonfixes/actions/runs/27474295512/job/81217868083.

--- gamefixes-umu/umu-zwift.py
+++ gamefixes-umu/umu-zwift.py
@@ -3,10 +3,15 @@
 import os
 from protonfixes import util
 
+
 # Necessary for dotnet installations
 # see: https://github.com/Open-Wine-Components/umu-protonfixes/pull/557
 def early() -> None:
     os.environ['PROTON_DLL_COPY'] = '*'
 
+
 def main() -> None:
-    util.protontricks('corefonts dotnet48 d3dcompiler_47 webview2')
+    util.protontricks('corefonts')
+    util.protontricks('dotnet48')
+    util.protontricks('d3dcompiler_47')
+    util.protontricks('webview2')
