5282529d03a79279598b8c0f28b5b4e87d2951fb
JellyDude <SinnohConfirmed@users.noreply.github.com>
2026-06-16 17:11:26 -0400

    Update existing gamefixes to use PROTON_OLD_GL_STRING (#579)
    * add fixes for various id tech games
    
    * add game fix for GOG version of Quake
    
    * update game fixes to use PROTON_OLD_GL_STRING
    
    * removed extra space from scripts
    
    * update existing gamefixes to use PROTON_OLD_GL_STRING

--- gamefixes-steam/2620.py
+++ gamefixes-steam/2620.py
@@ -3,8 +3,7 @@
 from protonfixes import util
 
 
-def main() -> None:
+def early() -> None:
     """Set Mesa env vars"""
     # https://github.com/ValveSoftware/Proton/pull/1423/commits/1a1d25c7d95691e37c94aea4e5f94e0c917aba6f
-    util.set_environment('MESA_EXTENSION_MAX_YEAR', '2003')
-    util.set_environment('__GL_ExtensionStringVersion', '17700')
+    util.set_environment('PROTON_OLD_GL_STRING', '1')
