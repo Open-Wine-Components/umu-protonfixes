44d8004c93ce901910a79681ab84daaa496274bc
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2026-06-17 14:51:02 -0700

    gamefixes-{gog,steam}: add entries for Ground Control II
    Follow up to commit c80c9948b6c341f0b192c27141200cac31354a74

--- gamefixes-gog/umu-254840.py
+++ gamefixes-gog/umu-254840.py
@@ -1,7 +1,1 @@
-"""Game fix for Ground Control 2"""
-
-from protonfixes import util
-
-
-def main() -> None:
-    util.protontricks('verdana')
+../gamefixes-steam/254840.py
---
+++ gamefixes-steam/254840.py
@@ -0,0 +1,7 @@
+"""Game fix for Ground Control 2"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    util.protontricks('verdana')
