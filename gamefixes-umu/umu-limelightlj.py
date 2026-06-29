c43c368d83120cc1bcc1f1df52c094f75af5744c
GloriousEggroll <gloriouseggroll@gmail.com>
2026-06-29 01:06:17 -0600

    add fixup override for limelight lemonade jam

--- gamefixes-umu/umu-limelightlj.py
+++ gamefixes-umu/umu-limelightlj.py
@@ -1,1 +1,8 @@
-umu-tenshisz.py
+"""Game fix for Limelight Lemonade Jam"""
+
+from protonfixes import util
+
+
+def main() -> None:
+    util.winedll_override('version', util.OverrideOrder.NATIVE_BUILTIN)
+    util.append_argument('-vomstyle=layer')
