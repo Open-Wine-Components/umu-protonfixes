bda08de62bf957ce8b840dc488e4ad721e8b2ad0
JellyDude <SinnohConfirmed@users.noreply.github.com>
2026-08-16 10:58:47 -0400

    Update comment

--- gamefixes-gog/umu-1207658752.py
+++ gamefixes-gog/umu-1207658752.py
@@ -4,7 +4,7 @@ from protonfixes import util
 
 def main() -> None:
     # Fix game not launching
-    # Requires setting the Windows version to Windows 95 or 98. Winetricks doesn't support setting this on a 64-bit prefix but this can be worked around using the registry
+    # Requires setting the Windows version to Windows 95 or 98. Winetricks doesn't support setting this on a 64-bit prefix but this can be worked around by editing the registry directly
     util.regedit_add(
         'HKEY_CURRENT_USER\\Software\\Wine',
         'Version',
