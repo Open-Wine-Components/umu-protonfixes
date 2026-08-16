7afd54f1c4549fe662dfb722dc4fc15b1cbeda3c
JellyDude <SinnohConfirmed@users.noreply.github.com>
2026-08-16 10:55:08 -0400

    Add fix for Tom Clancy's Rainbow Six (GOG)
    Fixes game not starting and music not playing on GOG version.

---
+++ gamefixes-gog/umu-1207658752.py
@@ -0,0 +1,15 @@
+"""Game fix for Tom Clancy's Rainbow Six"""
+
+from protonfixes import util
+
+def main() -> None:
+    # Fix game not launching
+    # Requires setting the Windows version to Windows 95 or 98. Winetricks doesn't support setting this on a 64-bit prefix but this can be worked around using the registry
+    util.regedit_add(
+        'HKEY_CURRENT_USER\\Software\\Wine',
+        'Version',
+        'REG_SZ',
+        'win98',
+    )
+    # Fix in-game music not playing
+    util.winedll_override('winmm', util.OverrideOrder.NATIVE_BUILTIN)
